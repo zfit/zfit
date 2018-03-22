30b4c65d13d9586935e7ace269e12d69a3b7ed7c
Albert Puig <albert.puig@cern.ch>
2018-03-22 11:32:20 +0100

    Update tests.

--- tests/test_zfit.py
+++ tests/test_zfit.py
@@ -5,18 +5,4 @@
 
 import pytest
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    import requests
-    return requests.get('https://github.com/zfit/zfit')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    from bs4 import BeautifulSoup
-    assert 'GitHub' in BeautifulSoup(response.content, "lxml").title.string
+import zfit
