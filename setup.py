94bddf1e25c39e3870660804db01995e9df5811b
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-03-29 01:24:18 +0200

    Add requirements and setup extension

--- setup.py
+++ setup.py
@@ -2,36 +2,50 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-
+import os
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'requirements.txt')) as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open(os.path.join(here, 'requirements_dev.txt')) as requirements_dev_file:
+    requirements_dev = requirements_dev_file.read().splitlines()
+
+with open(os.path.join(here, 'README.rst')) as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open(os.path.join(here, 'HISTORY.rst')) as history_file:
     history = history_file.read()
 
-requirements = [ ]
+# split the developer requirements into setup and test requirements
+if not requirements_dev.count("") == 1 or requirements_dev.index("") == 0:
+    raise SyntaxError("requirements_dev.txt has the wrong format: setup and test "
+                      "requirements have to be separated by one blank line.")
+requirements_dev_split = requirements_dev.index("")
 
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
+setup_requirements = requirements_dev[:requirements_dev_split]
+test_requirements = requirements_dev[requirements_dev_split+1:]
 
 setup(
     author="zfit",
     author_email='zfit@physik.uzh.ch',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ],
+        'Topic :: Scientific/Engineering :: Physics',
+        ],
     description="Zürich fitter.",
     install_requires=requirements,
     license="GNU General Public License v3",
@@ -39,6 +53,7 @@ setup(
     include_package_data=True,
     keywords='zfit',
     name='zfit',
+    python_requires=">=2.7",
     packages=find_packages(include=['zfit']),
     setup_requires=setup_requirements,
     test_suite='tests',
