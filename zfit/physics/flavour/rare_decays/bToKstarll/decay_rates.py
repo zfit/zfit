810e2adfd6a7beb1601cb273a38985f039bc71ec
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-03-30 18:09:02 +0200

    Reformat leftout file

--- zfit/physics/flavour/rare_decays/bToKstarll/decay_rates.py
+++ zfit/physics/flavour/rare_decays/bToKstarll/decay_rates.py
@@ -31,17 +31,17 @@ def d4Gamma(phsp, x, ml):
     cos2ThetaL = (2.0 * cosThetaL * cosThetaL - 1.0)
 
     fullPDF = ((3.0 / (8.0 * interface.Pi())) * (
-                                      ang.J1s(q2, ml)   * sinTheta2K
-                                      + ang.J1c(q2, ml) * cosTheta2K
-                                      + ang.J2s(q2, ml) * cos2ThetaL  * sinTheta2K
-                                      + ang.J2c(q2, ml) * cos2ThetaL  * cosTheta2K
-                                      + ang.J3(q2, ml)  * tf.cos(2.0  * phi) * sinTheta2K * sinTheta2L
-                                      + ang.J4(q2, ml)  * tf.cos(phi) * sin2ThetaK * sin2ThetaL
-                                      + ang.J5(q2, ml)  * tf.cos(phi) * sin2ThetaK * sinThetaL
-                                      + ang.J6s(q2, ml) * sinTheta2K  * cosThetaL
-                                      + ang.J7(q2, ml)  * sin2ThetaK  * sinThetaL * tf.sin(phi)
-                                      + ang.J8(q2, ml)  * sin2ThetaK  * sin2ThetaL * tf.sin(phi)
-                                      + ang.J9(q2, ml)  * sinTheta2K  * sinTheta2L * tf.sin(2. * phi)))
+        ang.J1s(q2, ml) * sinTheta2K
+        + ang.J1c(q2, ml) * cosTheta2K
+        + ang.J2s(q2, ml) * cos2ThetaL * sinTheta2K
+        + ang.J2c(q2, ml) * cos2ThetaL * cosTheta2K
+        + ang.J3(q2, ml) * tf.cos(2.0 * phi) * sinTheta2K * sinTheta2L
+        + ang.J4(q2, ml) * tf.cos(phi) * sin2ThetaK * sin2ThetaL
+        + ang.J5(q2, ml) * tf.cos(phi) * sin2ThetaK * sinThetaL
+        + ang.J6s(q2, ml) * sinTheta2K * cosThetaL
+        + ang.J7(q2, ml) * sin2ThetaK * sinThetaL * tf.sin(phi)
+        + ang.J8(q2, ml) * sin2ThetaK * sin2ThetaL * tf.sin(phi)
+        + ang.J9(q2, ml) * sinTheta2K * sinTheta2L * tf.sin(2. * phi)))
 
     return fullPDF
 
@@ -53,4 +53,4 @@ def dGamma_dq2(q2, ml):
     """
 
     return 2. * ang.J1s(q2, ml) + ang.J1c(q2, ml) - 1. / 3. * (
-            2. * ang.J2s(q2, ml) + ang.J2c(q2, ml))
+        2. * ang.J2s(q2, ml) + ang.J2c(q2, ml))
--- zfit/physics/flavour/rare_decays/bToKstarll/observables.py
+++ zfit/physics/flavour/rare_decays/bToKstarll/observables.py
@@ -5,76 +5,92 @@ import tensorflow as tf
 from . import angular_coefficients as ang
 from . import decay_rates
 
-def S1c(q2,ml):
-    return ang.J1c(q2,ml) / decay_rates.dGamma_dq2(q2)
 
-def S1s(q2,ml):
-    return ang.J1s(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S2c(q2,ml):
-    return ang.J2c(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S2s(q2,ml):
-    return ang.J2s(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S3(q2,ml):
-    return ang.J3(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S4(q2,ml):
-    return ang.J4(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S5(q2,ml):
-    return ang.J5(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S6s(q2,ml):
-    return ang.J6s(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def AFB(q2,ml):
-    return 3./4. * S6s(q2,ml)
-
-def S7(q2,ml):
-    return ang.J7(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S8(q2,ml):
-    return ang.J8(q2,ml) / decay_rates.dGamma_dq2(q2)
-
-def S9(q2,ml):
-    return ang.J9(q2,ml) / decay_rates.dGamma_dq2(q2)
+def S1c(q2, ml):
+    return ang.J1c(q2, ml) / decay_rates.dGamma_dq2(q2)
 
 
+def S1s(q2, ml):
+    return ang.J1s(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S2c(q2, ml):
+    return ang.J2c(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S2s(q2, ml):
+    return ang.J2s(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S3(q2, ml):
+    return ang.J3(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S4(q2, ml):
+    return ang.J4(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S5(q2, ml):
+    return ang.J5(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S6s(q2, ml):
+    return ang.J6s(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def AFB(q2, ml):
+    return 3. / 4. * S6s(q2, ml)
+
+
+def S7(q2, ml):
+    return ang.J7(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S8(q2, ml):
+    return ang.J8(q2, ml) / decay_rates.dGamma_dq2(q2)
+
+
+def S9(q2, ml):
+    return ang.J9(q2, ml) / decay_rates.dGamma_dq2(q2)
 
 
 # Optimized angular base
 
-def FL(q2,ml):
-    return S1c(q2,ml) - 1./3. * S2c(q2,ml)
-
-def P1(q2,ml):
-    return 2. * S3(q2,ml) / (1. - FL(q2,ml) )
-
-def P2(q2,ml):
-    return 1./2. * S6s(q2,ml) / (1. - FL(q2,ml) )
-
-def P3(q2,ml):
-    return -1. * S9(q2,ml) / (1. - FL(q2,ml) )
-
-def P4p(q2,ml):
-    fl = FL(q2,ml)
-    return S4(q2,ml) / tf.sqrt( fl * (1. - fl) )
-
-def P5p(q2,ml):
-    fl = FL(q2,ml)
-    return S5(q2,ml) / tf.sqrt( fl * (1. - fl) )
-
-def P6p(q2,ml):
-    fl = FL(q2,ml)
-    return S7(q2,ml) / tf.sqrt( fl * (1. - fl) )
-
-def P8p(q2,ml):
-    fl = FL(q2,ml)
-    return S8(q2,ml) / tf.sqrt( fl * (1. - fl) )
-
-def AT2(q2,ml):
-    return P1(q2,ml)
+def FL(q2, ml):
+    return S1c(q2, ml) - 1. / 3. * S2c(q2, ml)
 
 
+def P1(q2, ml):
+    return 2. * S3(q2, ml) / (1. - FL(q2, ml))
+
+
+def P2(q2, ml):
+    return 1. / 2. * S6s(q2, ml) / (1. - FL(q2, ml))
+
+
+def P3(q2, ml):
+    return -1. * S9(q2, ml) / (1. - FL(q2, ml))
+
+
+def P4p(q2, ml):
+    fl = FL(q2, ml)
+    return S4(q2, ml) / tf.sqrt(fl * (1. - fl))
+
+
+def P5p(q2, ml):
+    fl = FL(q2, ml)
+    return S5(q2, ml) / tf.sqrt(fl * (1. - fl))
+
+
+def P6p(q2, ml):
+    fl = FL(q2, ml)
+    return S7(q2, ml) / tf.sqrt(fl * (1. - fl))
+
+
+def P8p(q2, ml):
+    fl = FL(q2, ml)
+    return S8(q2, ml) / tf.sqrt(fl * (1. - fl))
+
+
+def AT2(q2, ml):
+    return P1(q2, ml)
