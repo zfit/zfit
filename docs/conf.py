44df61486253fa59c7b477de4ba9ba718db09b23
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-04-01 21:34:43 +0200

    Extend CI with OSX, coverage, improve docs generation

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,9 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import zfit
+import sphinx_rtd_theme
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # -- General configuration ---------------------------------------------
 
@@ -84,7 +87,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
