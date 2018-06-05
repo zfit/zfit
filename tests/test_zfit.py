cf4767e39696a267ecf9f23c831ff4710dd724d0
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-06-05 23:34:27 +0200

    Add pdf base class

--- tests/test_zfit.py
+++ tests/test_zfit.py
@@ -4,6 +4,13 @@
 """Tests for `zfit` package."""
 from __future__ import print_function, division, absolute_import
 
+suppress_gpu = True
+if suppress_gpu:
+    import os
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 import pytest
 
 import zfit
--- zfit/core/pdf.py
+++ zfit/core/pdf.py
@@ -3,3 +3,19 @@ Definition of the pdf interface, base etc.
 """
 
 from __future__ import print_function, division, absolute_import
+
+
+class BaseDistribution(object):
+
+
+    def sample(self):
+        pass
+
+    def log_prob(self):
+        pass
+
+    def batch_shape_tensor(self):
+        pass
+
+    def event_shape_tensor(self):
+        pass
