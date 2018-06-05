3f0a0b1af201eded1208ade21e978d0afad1ecfa
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-06-05 23:38:13 +0200

    Create base pdf with minimum functions

--- zfit/core/pdf.py
+++ zfit/core/pdf.py
@@ -7,15 +7,14 @@ from __future__ import print_function, division, absolute_import
 
 class BaseDistribution(object):
 
-
-    def sample(self):
+    def sample(self, sample_shape=(), seed=None, name='sample'):
         pass
 
-    def log_prob(self):
+    def log_prob(self, value, name='log_prob'):
         pass
 
-    def batch_shape_tensor(self):
+    def batch_shape_tensor(self, name='batch_shape_tensor'):
         pass
 
-    def event_shape_tensor(self):
+    def event_shape_tensor(self, name='event_shape_tensor'):
         pass
