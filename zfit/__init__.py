7f4af1cc220f89880fcf64ad58dd9540940d4acc
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-12-19 16:11:45 +0100

    Add func to namespace

--- zfit/__init__.py
+++ zfit/__init__.py
@@ -6,7 +6,7 @@ __email__ = 'zfit@physik.uzh.ch'
 __version__ = '0.0.0'
 
 import zfit.ztf
-from . import pdf, minimize, loss, core, data
+from . import pdf, minimize, loss, core, data, func
 from .core.parameter import Parameter
 from .core.loss import _unbinned_nll_tf
 from .core.limits import Space, convert_to_space, supports
