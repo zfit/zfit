b4c05b80be6c21d461da679fce2d1d46c1c5ba2b
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-01-13 21:16:41 +0100

    WIP refactor minimizer, fit result

--- zfit/minimizers/baseminimizer.py
+++ zfit/minimizers/baseminimizer.py
@@ -16,38 +16,32 @@ from typing import Dict, List, Union, Optional
 
 import zfit
 from zfit import ztf
-from zfit.minimizers.fitresult import MinimizerState
+from zfit.core.interfaces import ZfitLoss
+from zfit.minimizers.fitresult import FitResult
 from zfit.util import ztyping
+from zfit.util.temporary import TemporarilySet
 
 
 class MinimizerInterface(object):
     """Define the minimizer interface."""
 
     @abc.abstractmethod
-    def minimize(self, params=None, sess=None):
+    def minimize(self, loss, params=None):
         raise NotImplementedError
 
-    def _minimize(self, params):
+    def _minimize(self, loss, params):
         raise NotImplementedError
 
-    def _minimize_with_step(self, params):
+    def _minimize_with_step(self, loss, params):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def edm(self):
+    def step(self, loss, params=None):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def fmin(self):
+    def _step_tf(self, loss, params):
         raise NotImplementedError
 
-    def step(self, params=None, sess=None):
-        raise NotImplementedError
-
-    def _step_tf(self, params):
-        raise NotImplementedError
-
-    def _step(self, params):
+    def _step(self, loss, params):
         raise NotImplementedError
 
     @property
@@ -58,235 +52,98 @@ class MinimizerInterface(object):
     def _tolerance(self):
         raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_state(self):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def hesse(self, params=None, sess=None):
-        raise NotImplementedError
-
-    def _hesse(self, params):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def error(self, params=None, sigma=1., sess=None):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def set_error_method(self, method):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def set_error_options(self, replace=False, **options):
-        raise NotImplementedError
-
-
-def _raises_error_method(*_, **__):
-    raise NotImplementedError("No error method specified or implemented as default")
-
 
 class BaseMinimizer(MinimizerInterface, pep487.PEP487Object):
     _DEFAULT_name = "BaseMinimizer"
 
-    def __init__(self, loss, params=None, tolerance=None, name=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, name=None, tolerance=None):
+        super().__init__()
         if name is None:
             name = self._DEFAULT_name
-        self._current_error_method = self._error_methods.get('default', _raises_error_method)
-        self._current_error_options = {}
-        self._minimizer_state = MinimizerState()
         self.name = name
         self.tolerance = tolerance
         self._sess = None
-        self._params = OrderedDict()
-        self.loss = loss
-        self.set_params(params)
 
-    def __init_subclass__(cls, **kwargs):
-        cls._error_methods = {'default': None}  # TODO: set default error method
-
-    @property
-    def edm(self):
-        """The estimated distance to the minimum.
-
-        Returns:
-            numeric
-        """
-        return self.get_state(copy=False).edm
-
-    @property
-    def fmin(self):
-        """Function value at the minimum.
-
-        Returns:
-            numeric
-        """
-        return self.get_state(copy=False).fmin
-
-    def hesse(self, params: ztyping.ParamsOrNameType = None, sess: ztyping.SessionType = None) -> Dict:
-        """Calculate and set for `params` the symmetric error using the Hessian matrix.
-
-        Args:
-            params (list(`zfit.FitParameters` or str)): The parameters or their names to calculate the
-                Hessian symmetric error.
-            sess (`tf.Session` or None): A TensorFlow session to use for the calculation.
-
-        Returns:
-            `dict`: A `dict` containing as keys the parameter names and as value a `dict` which
-                contains (next to probably more things) a key 'error', holding the calculated error.
-                Example: result['par1']['error'] -> the symmetric error on 'par1'
-        """
-        params = self._check_input_params(params)
-
-        with self._temp_sess(sess=sess):
-            errors = self._hesse(params=params)
-            for param in params:
-                param.error = errors[param.name]['error']
-
-            return errors
-
-    def _check_input_params(self, params, only_floating=True):
+    def _check_input_params(self, loss: ZfitLoss, params, only_floating=True):
         if isinstance(params, (str, tf.Variable)) or (not hasattr(params, "__len__") and params is not None):
             params = [params, ]
         if params is None or isinstance(params[0], str):
-            params = self.get_parameters(names=params, only_floating=only_floating)
+            params = loss.get_dependents(only_floating=only_floating)
         return params
 
-    def error(self, params: ztyping.ParamsOrNameType = None, sess: ztyping.SessionType = None) -> Dict:
-        """Calculate and set for `params` the asymmetric error using the set error method.
+    # def set_params(self, params: ztyping.ParamsType, update: bool = False):
+    #     """Set the parameters of the minimizer.
+    #
+    #     If `None`, the parameters are empty. If a dictionary is given,
+    #
+    #     Args:
+    #         params (list, dict, None): The parameters.
+    #         update (bool): If True, add (or overwrite if existing) the `params` to the currently
+    #             stored parameters.
+    #     """
+    #     if params is None:
+    #         if update:
+    #             raise ValueError("Cannot specify `None` as params *and* set `update` to True.")
+    #         self._params = OrderedDict()
+    #         return
+    #     if not hasattr(params, "__len__"):
+    #         params = (params,)
+    #
+    #     if not update:  # overwrite: create empty instance
+    #         self._params = OrderedDict()
+    #     if isinstance(params, dict):
+    #         self._params.update(params)
+    #     else:
+    #         for param in params:
+    #             self._params[param.name] = param
 
-        Args:
-            params (list(`zfit.FitParameters` or str)): The parameters or their names to calculate the
-                 errors. If `params` is `None`, use all *floating* parameters.
-            sess (`tf.Session` or None): A TensorFlow session to use for the calculation.
+    # @contextlib.contextmanager
+    # def _temp_set_parameters(self, params):
+    #     old_params = self._params
+    #     try:
+    #         self.set_params(params)
+    #         yield params
+    #     finally:
+    #         self.set_params(old_params)
 
-        Returns:
-            `dict`: A `dict` containing as keys the parameter names and as value a `dict` which
-                contains (next to probably more things) two keys 'lower_error' and 'upper_error',
-                holding the calculated errors.
-                Example: result['par1']['upper_error'] -> the asymmetric upper error of 'par1'
-        """
-        params = self._check_input_params(params)
-        with self._temp_sess(sess=sess):
-            error_method = self._current_error_method
-            errors = error_method(params=params, **self._current_error_options)
-            for param in params:
-                param.lower_error = errors[param.name]['lower_error']
-                param.upper_error = errors[param.name]['upper_error']
-            return errors
+    def set_sess(self, sess):
+        value = sess
 
-    def set_error_method(self, method):
-        if isinstance(method, str):
-            try:
-                method = self._error_methods[method]
-            except AttributeError:
-                raise AttributeError("The error method '{}' is not registered with the minimizer.".format(method))
-        elif callable(method):
-            self._current_error_method = method
-        else:
-            raise ValueError("Method {} is neither a valid method name nor a callable function.".format(method))
+        def getter():
+            return self._sess  # use private attribute! self.sess creates default session
 
-    def set_error_options(self, replace: bool = False, **options):
-        """Specify the options for the `error` calculation.
+        def setter(value):
+            self.sess = value
 
-        Args:
-            replace (bool): If True, replace the current options. If False, only update
-                (add/overwrite existing).
-            **options (keyword arguments): The keyword arguments that will be given to `error`.
-        """
-        if replace:
-            self._current_error_options = {}
-        self._current_error_options.update(options)
+        return TemporarilySet(value=value, setter=setter, getter=getter)
 
-    def get_state(self, copy: bool = True) -> MinimizerState:
-        """Return the current state containing parameters, edm, fmin etc.
-
-        Args:
-            copy (bool): If True, return a copy of the (internal) state. Equivalent to a fit result.
-                If False, a reference is returned and the object will change if another method of
-                the minimizer is invoked (such as `error`, `minimize` etc.)
-
-        Returns:
-
-        """
-        import copy as copy_module
-        state = self._minimizer_state
-        if copy:
-            state = copy_module.deepcopy(state)
-        return state
-
-    def set_params(self, params: ztyping.ParamsType, update: bool = False):
-        """Set the parameters of the minimizer.
-
-        If `None`, the parameters are empty. If a dictionary is given,
-
-        Args:
-            params (list, dict, None): The parameters.
-            update (bool): If True, add (or overwrite if existing) the `params` to the currently
-                stored parameters.
-        """
-        if params is None:
-            if update:
-                raise ValueError("Cannot specify `None` as params *and* set `update` to True.")
-            self._params = OrderedDict()
-            return
-        if not hasattr(params, "__len__"):
-            params = (params,)
-
-        if not update:  # overwrite: create empty instance
-            self._params = OrderedDict()
-        if isinstance(params, dict):
-            self._params.update(params)
-        else:
-            for param in params:
-                self._params[param.name] = param
-
-    @contextlib.contextmanager
-    def _temp_set_parameters(self, params):
-        old_params = self._params
-        try:
-            self.set_params(params)
-            yield params
-        finally:
-            self.set_params(old_params)
-
-    @contextlib.contextmanager
-    def _temp_sess(self, sess):
-        old_sess = self.sess
-        try:
-            self.sess = sess
-            yield sess
-        finally:
-            self.sess = old_sess
-
-    def get_parameters(self, names: Optional[Union[List[str], str]] = None,
-                       only_floating: bool = True) -> List['Parameter']:  # TODO: automatically set?
-        """Return the parameters. If it is empty, automatically set and return all trainable variables.
-
-        Args:
-            names (str, list(str)): The names of the parameters to return.
-            only_floating (bool): If True, return only the floating parameters.
-
-        Returns:
-            list(`zfit.FitParameters`):
-        """
-        if not self._params:
-            raise RuntimeError("Currently need to pass explicitely the parameters.")
-            self.set_params(params=tf.trainable_variables())
-        if isinstance(names, str):
-            names = (names,)
-        if names is not None:
-            missing_names = set(names).difference(self._params.keys())
-            if missing_names:
-                raise KeyError("The following names are not valid parameter names")
-            params = [self._params[name] for name in names]
-        else:
-            params = list(self._params.values())
-
-        if only_floating:
-            params = self._filter_floating_params(params=params)
-        return params
+    # def get_parameters(self, names: Optional[Union[List[str], str]] = None,
+    #                    only_floating: bool = True) -> List['Parameter']:  # TODO: automatically set?
+    #     """Return the parameters. If it is empty, automatically set and return all trainable variables.
+    #
+    #     Args:
+    #         names (str, list(str)): The names of the parameters to return.
+    #         only_floating (bool): If True, return only the floating parameters.
+    #
+    #     Returns:
+    #         list(`zfit.FitParameters`):
+    #     """
+    #     if not self._params:
+    #         raise RuntimeError("Currently need to pass explicitely the parameters.")
+    #         self.set_params(params=tf.trainable_variables())
+    #     if isinstance(names, str):
+    #         names = (names,)
+    #     if names is not None:
+    #         missing_names = set(names).difference(self._params.keys())
+    #         if missing_names:
+    #             raise KeyError("The following names are not valid parameter names")
+    #         params = [self._params[name] for name in names]
+    #     else:
+    #         params = list(self._params.values())
+    #
+    #     if only_floating:
+    #         params = self._filter_floating_params(params=params)
+    #     return params
 
     @staticmethod
     def _filter_floating_params(params):
@@ -349,7 +206,7 @@ class BaseMinimizer(MinimizerInterface, pep487.PEP487Object):
         # TODO: implement if initial val not given
         return values
 
-    def step(self, params: ztyping.ParamsOrNameType = None):
+    def step(self, loss, params: ztyping.ParamsOrNameType = None):
         """Perform a single step in the minimization (if implemented).
 
         Args:
@@ -363,7 +220,7 @@ class BaseMinimizer(MinimizerInterface, pep487.PEP487Object):
         params = self._check_input_params(params)
         return self._step(params=params)
 
-    def minimize(self, params: ztyping.ParamsOrNameType = None, sess: ztyping.SessionType = None) -> "TODO":
+    def minimize(self, loss, params: ztyping.ParamsOrNameType = None) -> "FitResult":
         """Fully minimize the `loss` with respect to `params` using `sess`.
 
         Args:
@@ -372,50 +229,48 @@ class BaseMinimizer(MinimizerInterface, pep487.PEP487Object):
             sess (`tf.Session`): The session to use.
 
         Returns:
-            `MinimizerState`: The status of the minimizer, equivalent to a fit result.
+            `FitResult`: The fit result.
         """
-        with self._temp_sess(sess=sess):
-            params = self._check_input_params(params)
-            return self._hook_minimize(params=params)
+        params = self._check_input_params(loss=loss, params=params)
+        return self._hook_minimize(loss=loss, params=params)
 
-    def _hook_minimize(self, params):
-        return self._call_minimize(params=params)
+    def _hook_minimize(self, loss, params):
+        return self._call_minimize(loss=loss, params=params)
 
-    def _call_minimize(self, params):
+    def _call_minimize(self, loss, params):
         try:
-            return self._minimize(params=params)
+            return self._minimize(loss=loss, params=params)
         except NotImplementedError as error:
             try:
-                return self._minimize_with_step(params=params)
+                return self._minimize_with_step(loss=loss, params=params)
             except NotImplementedError:
                 raise error
 
-    def _minimize_with_step(self, params):  # TODO improve
+    def _minimize_with_step(self, loss, params):  # TODO improve
         changes = collections.deque(np.ones(10))
         last_val = -10
         cur_val = 9999999
         try:
-            step = self._step_tf(params=params)
+            step = self._step_tf(loss=loss, params=params)
         except NotImplementedError:
             step_fn = self.step
         else:
-            def step_fn(params):
+            def step_fn(loss, params):
                 return self.sess.run(step)
         while sum(sorted(changes)[-3:]) > self.tolerance:  # TODO: improve condition
-            _ = step_fn(params=params)
+            _ = step_fn(loss=loss, params=params)
             changes.popleft()
             changes.append(abs(cur_val - last_val))
             last_val = cur_val
-            cur_val = self.sess.run(self.loss.value())  # TODO: improve...
+            cur_val = self.sess.run(loss.value())  # TODO: improve...
         fmin = cur_val
         edm = -999  # TODO: get edm
         status = {}  # TODO: create status
 
-        self.get_state(copy=False)._set_new_state(params=params, edm=edm, fmin=fmin, status=status)
+        # TODO(Mayou36): create FitResult
         # HACK remove the line, `get_state()` currently cannot return Tensors
         return fmin
         # HACK END
-        return self.get_state()
 
 
 # WIP below
--- zfit/minimizers/fitresult.py
+++ zfit/minimizers/fitresult.py
@@ -3,19 +3,15 @@ from collections import OrderedDict
 from ..util.exception import NotMinimizedError
 
 
-class MinimizerState:
+class FitResult:
 
-    def __init__(self):
-        self._parameters = None
-        self._edm = None
-        self._fmin = None
-        self._status = None
-
-    def _set_new_state(self, params, edm, fmin, status):
-        self._set_parameters(params)
-        self._set_edm(edm)
-        self._set_fmin(fmin)
-        self._set_status(status)
+    def __init__(self, params, edm, fmin, status, loss, minimizer):
+        self._parameters = params
+        self._edm = edm
+        self._fmin = fmin
+        self._status = status
+        self._loss = loss
+        self._minimizer = minimizer
 
     def _set_parameters(self, params):  # TODO: define exactly one way to do it
         try:
@@ -32,29 +28,122 @@ class MinimizerState:
     @property
     def edm(self):
         edm = self._edm
-        if edm is None:
-            raise NotMinimizedError("No minimization performed so far. No edm set.")
         return
-
-    def _set_edm(self, edm):
-        self._edm = edm
 
     @property
     def fmin(self):
         fmin = self._fmin
-        if fmin is None:
-            raise NotMinimizedError("No minimization performed so far. No fmin set.")
         return fmin
-
-    def _set_fmin(self, fmin):
-        self._fmin = fmin
 
     @property
     def status(self):
         status = self._status
-        if status is None:
-            raise NotMinimizedError("No minimization performed so far. No status set.")
         return status
 
-    def _set_status(self, status):
-        self._status = status
+    # # just copy pasted
+    # @abc.abstractmethod
+    # def hesse(self, params=None, sess=None):
+    #     raise NotImplementedError
+    #
+    # def _hesse(self, params):
+    #     raise NotImplementedError
+    #
+    # @abc.abstractmethod
+    # def error(self, params=None, sigma=1., sess=None):
+    #     raise NotImplementedError
+    #
+    # @abc.abstractmethod
+    # def set_error_method(self, method):
+    #     raise NotImplementedError
+    #
+    # @abc.abstractmethod
+    # def set_error_options(self, replace=False, **options):
+    #     raise NotImplementedError
+    #
+    # def _raises_error_method(*_, **__):
+    #     raise NotImplementedError("No error method specified or implemented as default")
+    #
+    # @property
+    # def edm(self):
+    #     """The estimated distance to the minimum.
+    #
+    #     Returns:
+    #         numeric
+    #     """
+    #     return self.get_state(copy=False).edm
+    #
+    # @property
+    # def fmin(self):
+    #     """Function value at the minimum.
+    #
+    #     Returns:
+    #         numeric
+    #     """
+    #     return self.get_state(copy=False).fmin
+    #
+    # def hesse(self, params: ztyping.ParamsOrNameType = None, sess: ztyping.SessionType = None) -> Dict:
+    #     """Calculate and set for `params` the symmetric error using the Hessian matrix.
+    #
+    #     Args:
+    #         params (list(`zfit.FitParameters` or str)): The parameters or their names to calculate the
+    #             Hessian symmetric error.
+    #         sess (`tf.Session` or None): A TensorFlow session to use for the calculation.
+    #
+    #     Returns:
+    #         `dict`: A `dict` containing as keys the parameter names and as value a `dict` which
+    #             contains (next to probably more things) a key 'error', holding the calculated error.
+    #             Example: result['par1']['error'] -> the symmetric error on 'par1'
+    #     """
+    #     params = self._check_input_params(params)
+    #
+    #     with self.set_sess(sess=sess):
+    #         errors = self._hesse(params=params)
+    #         for param in params:
+    #             param.error = errors[param.name]['error']
+    #
+    #         return errors
+    # def error(self, params: ztyping.ParamsOrNameType = None, sess: ztyping.SessionType = None) -> Dict:
+    #     """Calculate and set for `params` the asymmetric error using the set error method.
+    #
+    #     Args:
+    #         params (list(`zfit.FitParameters` or str)): The parameters or their names to calculate the
+    #              errors. If `params` is `None`, use all *floating* parameters.
+    #         sess (`tf.Session` or None): A TensorFlow session to use for the calculation.
+    #
+    #     Returns:
+    #         `dict`: A `dict` containing as keys the parameter names and as value a `dict` which
+    #             contains (next to probably more things) two keys 'lower_error' and 'upper_error',
+    #             holding the calculated errors.
+    #             Example: result['par1']['upper_error'] -> the asymmetric upper error of 'par1'
+    #     """
+    #     params = self._check_input_params(params)
+    #     with self.set_sess(sess=sess):
+    #         error_method = self._current_error_method
+    #         errors = error_method(params=params, **self._current_error_options)
+    #         for param in params:
+    #             param.lower_error = errors[param.name]['lower_error']
+    #             param.upper_error = errors[param.name]['upper_error']
+    #         return errors
+    #
+    # def set_error_method(self, method):
+    #     if isinstance(method, str):
+    #         try:
+    #             method = self._error_methods[method]
+    #         except AttributeError:
+    #             raise AttributeError("The error method '{}' is not registered with the minimizer.".format(method))
+    #     elif callable(method):
+    #         self._current_error_method = method
+    #     else:
+    #         raise ValueError("Method {} is neither a valid method name nor a callable function.".format(method))
+    #
+    # def set_error_options(self, replace: bool = False, **options):
+    #     """Specify the options for the `error` calculation.
+    #
+    #     Args:
+    #         replace (bool): If True, replace the current options. If False, only update
+    #             (add/overwrite existing).
+    #         **options (keyword arguments): The keyword arguments that will be given to `error`.
+    #     """
+    #     if replace:
+    #         self._current_error_options = {}
+    #     self._current_error_options.update(options)
