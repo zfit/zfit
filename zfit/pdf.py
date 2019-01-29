59b807e48c4575c4144d15374d069b9a5e65f7f1
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-01-29 15:00:43 +0100

    Add CrystalBall pdf to namespace

--- zfit/pdf.py
+++ zfit/pdf.py
@@ -1,5 +1,6 @@
 from .core.basepdf import BasePDF
 from .models.basic import Exponential
+from .models.physics import CrystalBallPDF
 from .models.dist_tfp import Gauss, Uniform, WrapDistribution
 from .models.functor import ProductPDF, SumPDF
 # from .models.special import
