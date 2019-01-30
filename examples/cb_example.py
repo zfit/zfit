Fix CB function, add `safe_where` for correct gradient propagation.

examples/cb_example.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import zfit
 # Wrapper for some tensorflow functionality
 from zfit import ztf
+from zfit.minimizers.minimizers_scipy import ScipyMinimizer
 
 print("TensorFlow version:", tf.__version__)
 Bmass = zfit.Space('M_ppiKS', limits=(5469, 5938))
@@ -17,35 +18,54 @@ branches = ['M_ppiKS']
 data = zfit.data.Data.from_root(path=path, treepath=treepath, branches=branches)
 
 mu = zfit.Parameter("mu", 5619, 5500, 5650)
-sigma = zfit.Parameter("sigma", 30, 0, 300)
+# mu = zfit.Parameter("mu", 5619)
+mu2 = zfit.Parameter("mu2", 5619, 5500, 5650)
+# mu2 = zfit.Parameter("mu2", 5619)
+sigma = zfit.Parameter("sigma", 60, 0, 300)
+# sigma = zfit.Parameter("sigma", 60)
+sigma2 = zfit.Parameter("sigma2", 60, 0, 300)
+# sigma2 = zfit.Parameter("sigma2", 60)
 
-a0 = zfit.Parameter("a0", 1., 0., 10.)
+a0 = zfit.Parameter("a0", 1., 0., 1000.)
+# a0 = zfit.Parameter("a0", 1.)
 a1 = zfit.Parameter("a1", -1., -10., 0.)
+# a1 = zfit.Parameter("a1", -1.)
 
-n0 = zfit.Parameter("n0", 1, 0., 10.)
-n1 = zfit.Parameter("n1", 1., 0., 10.)
+n0 = zfit.Parameter("n0", 1, 0, 10.)
+# n0 = zfit.Parameter("n0", 1)
+n1 = zfit.Parameter("n1", 1., 0000001., 10.)
+# n1 = zfit.Parameter("n1", 1)
 
 frac = zfit.Parameter("frac", 0.5, 0., 1.)
 
 # gauss = zfit.pdf.Gauss(obs=Bmass, mu=mu, sigma=sigma)
 CB1 = zfit.pdf.CrystalBallPDF(obs=Bmass, mu=mu, sigma=sigma, alpha=a0, n=n0)
 CB2 = zfit.pdf.CrystalBallPDF(obs=Bmass, mu=mu, sigma=sigma, alpha=a1, n=n1)
+# CB1 = zfit.pdf.Gauss(obs=Bmass, mu=mu, sigma=sigma)
+# CB2 = zfit.pdf.Gauss(obs=Bmass, mu=mu2, sigma=sigma2)
 
 # Double Crystal Ball with a common mean and width
 DoubleCB = zfit.pdf.SumPDF(pdfs=[CB1, CB2], fracs=frac)
+# DoubleCB = CB1
 # CB = frac*CB1 +CB2
 
 # Create the negative log likelihood
 from zfit.core.loss import UnbinnedNLL
 
+probs = DoubleCB.pdf(x=np.linspace(5469, 5938, num=1000))
+
 nll = UnbinnedNLL(model=[DoubleCB], data=[data], fit_range=[Bmass])
-minimize_params = [mu, sigma, a0, a1, n0]
+# minimize_params = [mu, sigma, a0, a1]
+minimize_params = None
 # tf.gradients(nll.value(), minimize_params)
+# nll.value()
+# tf.add_check_numerics_ops()
 
 # Load and instantiate a tensorflow minimizer
 from zfit.minimizers.minimizer_minuit import MinuitMinimizer
 
-minimizer = MinuitMinimizer()
+# minimizer = MinuitMinimizer()
+minimizer = ScipyMinimizer()
 
 # Create the minimization graph to minimize
 minimum = minimizer.minimize(loss=nll, params=minimize_params)
@@ -54,4 +74,8 @@ minimum = minimizer.minimize(loss=nll, params=minimize_params)
 # params = minimum.get_parameters()
 params = minimum.params
 
+# error = minimum.error()
+# print(error)
+
 print(params)
+print(minimum.info)

zfit/models/functor.py
@@ -13,11 +13,10 @@ import tensorflow as tf
 import numpy as np
 
 from zfit import ztf
-from ..core.data import Data
 from ..core.interfaces import ZfitPDF, ZfitModel
 from ..core.limits import no_norm_range, supports
 from ..core.basepdf import BasePDF
-from ..core.parameter import Parameter
+from ..core.parameter import Parameter, convert_to_parameter
 from ..models.basefunctor import FunctorMixin
 from ..util import ztyping
 from ..util.container import convert_to_container
@@ -142,13 +141,15 @@ class SumPDF(BaseFunctor):
             name (str):
         """
         # Check user input, improve TODO
-        super().__init__(pdfs=pdfs, obs=obs, name=name)
-        pdfs = self.pdfs
+        set_yield_at_end = False
+        pdfs = convert_to_container(pdfs)
+        # pdfs = self.pdfs
+        self.pdfs = pdfs
         if len(pdfs) < 2:
             raise ValueError("Cannot build a sum of a single pdf")
         if fracs is not None:
             fracs = convert_to_container(fracs)
-            fracs = [ztf.convert_to_tensor(frac) for frac in fracs]
+            fracs = [convert_to_parameter(frac) for frac in fracs]
 
         # check if all extended
         extended_pdfs = self.pdfs_extended
@@ -224,12 +225,20 @@ class SumPDF(BaseFunctor):
         if extended:
             # yield_fracs = [yield_ / tf.reduce_sum(yields) for yield_ in yields]
             # self.fracs = yield_fracs
-            self._set_yield_inplace(tf.reduce_sum(yields))
+            set_yield_at_end = True
             self.fracs = [tf.constant(1, dtype=ztypes.float)] * len(self.pdfs)
         else:
             self.fracs = fracs
 
         self.pdfs = pdfs
+
+        parameters = OrderedDict()
+        for i, frac in enumerate(self.fracs):
+            parameters['frac_{}'.format(i)] = frac
+
+        super().__init__(pdfs=pdfs, obs=obs, parameters=parameters, name=name)
+        if set_yield_at_end:
+            self._set_yield_inplace(tf.reduce_sum(yields))
 
     @property
     def _n_dims(self):

zfit/models/physics.py
@@ -13,16 +13,20 @@ from ..util import ztyping
 
 
 def _powerlaw(x, a, k):
-    return a * x ** k
+    return a * tf.pow(x, k)
 
 
 def crystalball_func(x, mu, sigma, alpha, n):
     t = (x - mu) / sigma * tf.sign(alpha)
     abs_alpha = tf.abs(alpha)
-    a = (n / abs_alpha) ** n * tf.exp(-0.5 * tf.square(abs_alpha))
+    a = tf.pow((n / abs_alpha), n) * tf.exp(-0.5 * tf.square(alpha))
     b = (n / abs_alpha) - abs_alpha
-    cond = tf.greater_equal(t, -abs_alpha)
-    func = tf.where(cond, tf.exp(-0.5 * tf.square(t)), _powerlaw(b - t, a, -n))
+    cond = tf.less(t, -abs_alpha)
+    # func = tf.where(cond, tf.exp(-0.5 * tf.square(t)), _powerlaw(b - t, a, -n))
+    func = ztf.safe_where(cond,
+                          lambda t: _powerlaw(b - t, a, -n),
+                          lambda t: tf.exp(-0.5 * tf.square(t)),
+                          values=t, value_safer=lambda t: tf.ones_like(t) * (b - 2))
 
     return func
 

zfit/ztf/zextension.py
@@ -1,6 +1,6 @@
 import math as _mt
 
-from typing import Any
+from typing import Any, Callable
 
 try:
     from math import inf as _inf
@@ -67,5 +67,33 @@ def stack_x(values, axis: int = -1, name: str = "stack_x"):
 
 def convert_to_tensor(value, dtype=None, name=None, preferred_dtype=None):
     return tf.convert_to_tensor(value=value, dtype=dtype, name=name, preferred_dtype=preferred_dtype)
+
+
+def safe_where(condition: tf.Tensor, func: Callable, safe_func: Callable, values: tf.Tensor,
+               value_safer: Callable = tf.ones_like) -> tf.Tensor:
+    """Like `tf.where` but fixes gradient `NaN` if func produces `NaN` with certain `values`.
+
+    Args:
+        condition (`tf.Tensor`): Same argument as to `tf.where`, a boolean Tensor
+        func (Callable): Function taking `values` as argument and returning the tensor _in case
+            condition is True_. Equivalent `x` of `tf.where` but as function.
+        safe_func (Callable): Function taking `values` as argument and returning the tensor
+            _in case the condition is False_, Equivalent `y` of `tf.where` but as function.
+        values (`tf.Tensor`): Values to be evaluated either by `func` or `safe_func` depending on
+            `condition`.
+        value_safer (Callable): Function taking `values` as arguments and returns "safe" values
+            that won't cause troubles when given to`func` or by taking the gradient with respect
+            to `func(value_safer(values)`.
+
+    Returns:
+        `tf.Tensor`:
+
+
+
+    """
+    safe_x = tf.where(condition=condition, x=values, y=value_safer(values))
+    result = tf.where(condition=condition, x=func(safe_x), y=safe_func(values))
+    return result
+
 
 # reduce functions