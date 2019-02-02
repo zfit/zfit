Add chunked normalization, "bug" in TFP (#285)

tests/test_basePDF.py
@@ -23,11 +23,17 @@ obs1 = 'obs1'
 
 gauss_params1 = Gauss(mu=mu, sigma=sigma, obs=obs1, name="gauss_params1")
 
+sigma_true_param = zfit.Parameter('sigma_true123', sigma_true)
+sigma_true_param = zfit.Parameter('mu_true123', mu_true)
+
 
 class TestGaussian(zfit.core.basepdf.BasePDF):
 
     def _unnormalized_pdf(self, x, norm_range=False):
-        return tf.exp((-(x - mu_true) ** 2) / (2 * sigma_true ** 2))  # non-normalized gaussian
+        x = ztf.unstack_x(x)
+
+        return tf.exp((-(x - sigma_true_param) ** 2) / (
+            2 * sigma_true_param ** 2))  # non-normalized gaussian
 
 
 def true_gaussian_unnorm_func(x):

zfit/core/data.py
@@ -215,7 +215,7 @@ class Data(SessionHolderMixin, ZfitData, BaseDimensional, BaseObject):
             values = tf.expand_dims(values, -1)
         if len(values.shape.as_list()) == 1:
             values = tf.expand_dims(values, -1)
-        perm_indices = self.space.axes if self.space.axes != tuple(range(values.shape[1])) else False
+        perm_indices = self.space.axes if self.space.axes != tuple(range(values.shape[-1])) else False
 
         # permutate = perm_indices is not None
         if obs:

zfit/core/parameter.py
@@ -122,7 +122,7 @@ class ComposedResourceVariable(ResourceVariable):
 class ComposedVariable(tf.Variable, metaclass=type(tf.Variable)):
 
     def __init__(self, name: str, initial_value: tf.Tensor, **kwargs):
-        super().__init__(initial_value=initial_value, **kwargs)
+        super().__init__(initial_value=initial_value, **kwargs, use_resource=True)
         self._value_tensor = tf.convert_to_tensor(initial_value, preferred_dtype=ztypes.float)
         # self._name = name
 
@@ -320,7 +320,8 @@ class Parameter(SessionHolderMixin, ZfitParameterMixin, TFBaseVariable, BasePara
 
         # self.constraint = constraint
 
-        super().__init__(initial_value=init_value, dtype=dtype, name=name, constraint=constraint, **kwargs)
+        super().__init__(initial_value=init_value, dtype=dtype, name=name, constraint=constraint,
+                         **kwargs)
         if self.independent:
             tf.add_to_collection("zfit_independent", self)
         else: