c96a48be8899185104816c6b3234c9214a3124ef
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-03-19 17:34:34 +0100

    Add space.rst

--- zfit/__init__.py
+++ zfit/__init__.py
@@ -6,7 +6,8 @@ __email__ = 'zfit@physik.uzh.ch'
 __version__ = '0.0.0'
 
 __all__ = ["ztf", "constraint", "pdf", "minimize", "loss", "core", "data", "func",
-           "Parameter", "Space", "convert_to_space", "supports",
+           "Parameter", "ComposedParameter", "ComplexParameter", "convert_to_parameter",
+           "Space", "convert_to_space", "supports",
            "run", "settings"]
 
 from . import ztf
@@ -16,8 +17,9 @@ import tensorflow as tf
 tf.get_variable_scope().set_use_resource(True)
 
 from . import constraint, pdf, minimize, loss, core, data, func
-from .core.parameter import Parameter
+from .core.parameter import Parameter, ComposedParameter, ComplexParameter, convert_to_parameter
 from .core.limits import Space, convert_to_space, supports
+from .core.data import Data
 
 from .settings import run
 
