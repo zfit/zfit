e8f541703b0de1e5144d3de348c4d0a780eb9931
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-03-21 18:55:45 +0100

    Update version to 0.3.0

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zfit/zfit',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
     )
