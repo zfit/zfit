eff8dcdbf7d8c991f3da42ebef1604689114c100
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-03-21 04:05:34 +0100

    Adjust version to 0.2.3

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zfit/zfit',
-    version='0.2.0',
+    version='0.2.3',
     zip_safe=False,
     )
--- zfit/__init__.py
+++ zfit/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Top-level package for zfit."""
 
-__version__ = '0.2.0'
+__version__ = '0.2.3'
 
 __license__ = "BSD 3-Clause"
 __copyright__ = "Copyright 2018, zfit"
