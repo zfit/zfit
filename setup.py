a9b9647cf177f475ea6fe71abdb1e6aa906399b9
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-04-30 20:27:25 +0200

    Change version number to 0.3.2

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zfit/zfit',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
     )
