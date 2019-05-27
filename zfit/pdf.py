44e7d8fd6c3df2fa603b6d3c14d501c670fd2fbf
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2019-05-27 16:49:45 +0200

    Add BaseFunctor to PDF namespace

--- zfit/pdf.py
+++ zfit/pdf.py
@@ -4,7 +4,7 @@ from .core.basepdf import BasePDF
 from .models.basic import Exponential
 from .models.physics import CrystalBall, DoubleCB
 from .models.dist_tfp import Gauss, Uniform, WrapDistribution, TruncatedGauss
-from .models.functor import ProductPDF, SumPDF
+from .models.functor import ProductPDF, SumPDF, BaseFunctor
 from .models.special import ZPDF, SimplePDF, SimpleFunctorPDF
 
 __all__ = ['BasePDF',
