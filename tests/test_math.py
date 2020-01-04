Add test and fix numerical and automatic hessian

tests/test_math.py
@@ -13,12 +13,14 @@ from zfit.core.testing import setup_function, teardown_function, tester
 
 def test_numerical_gradient():
     param1 = zfit.Parameter('param1', 4.)
+    param2 = zfit.Parameter('param2', 5.)
+    param3 = zfit.Parameter('param3', 2.)
 
     def func1():
-        return param1 ** 2
+        return param1 * param2 ** 2 + param3 ** param1
 
-    num_gradients = numerical_gradient(func1, params=param1)
-    tf_gradients = automatic_gradient(func1, params=param1)
+    num_gradients = numerical_gradient(func1, params=[param1, param2, param3])
+    tf_gradients = automatic_gradient(func1, params=[param1, param2, param3])
     np.testing.assert_allclose(num_gradients, tf_gradients)
 
 
@@ -30,6 +32,6 @@ def test_numerical_hessian():
     def func1():
         return param1 * param2 ** 2 + param3 ** param1
 
-    num_hessian = numerical_hessian(func1, params=param1)
-    tf_hessian = automatic_hessian(func1, params=param1)
-    np.testing.assert_allclose(num_hessian, tf_hessian)
+    num_hessian = numerical_hessian(func1, params=[param1, param2, param3])
+    tf_hessian = automatic_hessian(func1, params=[param1, param2, param3])
+    np.testing.assert_allclose(num_hessian, tf_hessian, rtol=1e-5, atol=1e-10)

zfit/core/math.py
@@ -68,8 +68,6 @@ def numerical_hessian(func: Callable, params: Iterable[Parameter]) -> tf.Tensor:
     hesse_func = numdifftools.Hessian(wrapped_func)
     hessian = tf.py_function(hesse_func, inp=[param_vals],
                              Tout=tf.float64)
-    if hessian.shape == ():
-        hessian = tf.reshape(hessian, shape=(1,))
     hessian.set_shape((len(param_vals), len(param_vals)))
     for param, val in zip(params, original_vals):
         param.set_value(val)
@@ -143,6 +141,7 @@ def automatic_value_gradients(func: Callable, params: Iterable[Parameter]) -> [t
 def automatic_value_gradients_hessian(func: Callable, params: Iterable[Parameter]) -> [tf.Tensor, tf.Tensor, tf.Tensor]:
     with tf.GradientTape(persistent=False) as tape:
         loss, gradients = automatic_value_gradients(func=func, params=params)
+        gradients_tf = z.convert_to_tensor(gradients)
 
-    hessian = tape.gradient(gradients, sources=params)
+    hessian = z.convert_to_tensor(tape.jacobian(gradients_tf, sources=params))
     return loss, gradients, hessian