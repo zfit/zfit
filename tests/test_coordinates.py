486e473892df7de4f887692fc62fcf1e3445ca4c
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-01-13 12:24:37 +0100

    Extend coordinates test

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -1,7 +1,7 @@
 #  Copyright (c) 2020 zfit
+import numpy as np
 import pytest
 import tensorflow as tf
-import numpy as np
 
 from zfit import z
 from zfit.core.space_new import Coordinates
@@ -14,15 +14,49 @@ def test_reorder_x(graph):
     x = tf.constant([2, 3, 4, 1], dtype=tf.float64)
 
     def test():
-        coords = Coordinates(['a', 'b', 'c', 'd'])
-        x1 = coords.reorder_x(x, x_obs=['b', 'c', 'd', 'a'])
-        x2 = coords.reorder_x(x, func_obs=['d', 'a', 'b', 'c', ])
+        x_all = []
+        obs = ['a', 'b', 'c', 'd']
+        x_obs = ['b', 'c', 'd', 'a']
+        func_obs = ['d', 'a', 'b', 'c', ]
+        coords = Coordinates(obs)
+        x_all.append(coords.reorder_x(x, x_obs=x_obs))
+        x_all.append(coords.reorder_x(x, func_obs=func_obs))
 
-        return x1, x2
+        axes = [0, 1, 2, 3]
+        x_axes = [1, 2, 3, 0]
+        func_axes = [3, 0, 1, 2]
+        coords = Coordinates(axes=axes)
+        x_all.append(coords.reorder_x(x, x_axes=x_axes))
+        x_all.append(coords.reorder_x(x, func_axes=func_axes))
+
+        coords = Coordinates(obs, axes)
+        x_all.append(coords.reorder_x(x, x_obs=x_obs, x_axes=x_axes))
+        x_all.append(coords.reorder_x(x, func_obs=func_obs, func_axes=func_axes))
+
+        coords = Coordinates(obs, axes)
+        x_all.append(coords.reorder_x(x, x_obs=x_obs))
+        x_all.append(coords.reorder_x(x, func_obs=func_obs))
+
+        coords = Coordinates(obs)
+        x_all.append(coords.reorder_x(x, x_obs=x_obs, x_axes=x_axes))
+        x_all.append(coords.reorder_x(x, func_obs=func_obs, func_axes=func_axes))
+
+        with pytest.raises(ValueError):
+            coords.reorder_x(x, x_axes=x_axes)
+        with pytest.raises(ValueError):
+            coords.reorder_x(x, func_axes=func_axes)
+
+        coords = Coordinates(axes=axes)
+        with pytest.raises(ValueError):
+            coords.reorder_x(x, x_obs=x_obs)
+        with pytest.raises(ValueError):
+            coords.reorder_x(x, func_obs=func_obs)
+
+        return x_all
 
     if graph:
         test = z.function(test)
-    x1, x2 = test()
+    all_x = test()
     true_x = tf.constant([1, 2, 3, 4], dtype=tf.float64)
-    assert np.allclose(x1, true_x)
-    assert np.allclose(x2, true_x)
+    for x in all_x:
+        assert np.allclose(x, true_x)
