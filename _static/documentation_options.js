47a05d491d4587401d83b21424eb64f81b9ec828
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-21 22:41:48 +0000

    Deploy zfit/zfit to github.com/zfit/zfit.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.4.dev114+gb4bf4e3',
+    VERSION: '0.4.4.dev126+g6bc7429',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
