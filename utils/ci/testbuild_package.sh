6359ea511fc34886984a49c80fb64f441845c9d4
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-04-27 11:55:52 +0200

    Update cleaning

--- utils/ci/testbuild_package.sh
+++ utils/ci/testbuild_package.sh
@@ -3,5 +3,5 @@
 echo "============================ Building package for test ============================"
 python setup.py sdist bdist_wheel  2>&1 | tail -n 15
 twine check dist/*
-python setup.py clean # cleanup
+python setup.py clean --dist --eggs # cleanup
 echo "======================= Finished building package for test ========================"
