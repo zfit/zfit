3a7925a81f5f9b844b2341b0f4f72a97a285e4d1
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-07-01 20:33:00 +0200

    Fix GaussianConstraint docs to reflect actual code

--- zfit/core/constraint.py
+++ zfit/core/constraint.py
@@ -193,9 +193,9 @@ class GaussianConstraint(TFProbabilityConstraint):
 
 
         Args:
-            params (list(zfit.Parameter)): The parameters to constraint; corresponds to mu in the Gaussian
+            params (list(zfit.Parameter)): The parameters to constraint; corresponds to x in the Gaussian
                 distribution.
-            observation (numerical, list(numerical)): observed values of the parameter; corresponds to the x argument
+            observation (numerical, list(numerical)): observed values of the parameter; corresponds to mu
                 in the Gaussian distribution.
             uncertainty (numerical, list(numerical) or array/tensor): Uncertainties or covariance/error
                 matrix of the observed values. Can either be a single value, a list of values, an array or a tensor.
