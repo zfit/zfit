7c425a464ef738f8ed3c576c8645bf56c1c569d5
SebastianJL <johannes.lade@yahoo.com>
2020-07-02 11:08:07 +0200

    Reformat conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -22,17 +22,18 @@ needs_sphinx = '3.0.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.intersphinx',
-              'sphinx.ext.coverage',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.githubpages',
-              'sphinx.ext.napoleon',
-              'sphinx_autodoc_typehints',  # must be imported after napoleon to properly work.
-              # See https://github.com/agronholm/sphinx-autodoc-typehints/issues/15
-              'sphinx.ext.todo',
-              ]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',  # must be imported after napoleon to properly work.
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
+    # See https://github.com/agronholm/sphinx-autodoc-typehints/issues/15
+    'sphinx.ext.todo',
+]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -75,7 +76,6 @@ with open('subst_types.txt') as subst_types:
     rst_epilog = subst_types.read()
 
 html_favicon = "images/zfit-favicon.png"
-
 
 # -- Napoleon settings ---------------------------------------------
 
