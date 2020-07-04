cdc39805222462a04b018a6e8694cd1ff86bb135
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-07-04 16:53:20 +0200

    Add working example

--- zfit/serialization/zfit_repr.py
+++ zfit/serialization/zfit_repr.py
@@ -26,11 +26,11 @@ class ZfitArranger(ABC):
 
     @abstractmethod
     def dump(self, rep: ZfitRepr) -> Dict['str', Any]:
-        ...
+        return {'type': rep.uid}
 
     @abstractmethod
-    def load(self, string: str) -> ZfitRepr:
-        ...
+    def load(self, struct: Dict[str, Any]) -> ZfitRepr:
+        return {}
 
 
 class StrRepr(ZfitRepr):
@@ -42,8 +42,8 @@ class StrArranger(ZfitArranger):
         return rep.value
         # return obj.fields['value']
 
-    def load(self, string: str) -> StrRepr:
-        return StrRepr(uid='str', value=string)
+    def load(self, struct: str) -> StrRepr:
+        return StrRepr(uid='str', value=struct)
 
 
 class ParamRepr(ZfitRepr):
@@ -59,7 +59,14 @@ class ParamRepr(ZfitRepr):
 class ParamArranger(ZfitArranger):
 
     def dump(self, rep: ParamRepr) -> Dict['str', Any]:
-        return {'name': StrArranger().dump(rep.fields['name'])}
+        structure = super().dump(rep)
+        structure['name'] = StrArranger().dump(rep.fields['name'])
+        return structure
+
+    def load(self, struct: Dict[str, Any]) -> ZfitRepr:
+        struct = struct.copy()
+        rep = ParamRepr(uid=struct['type'], fields={'name': StrArranger().load(struct.pop('name'))})
+        return rep
 
 
 class FloatRepr(ZfitRepr):
@@ -71,8 +78,8 @@ class FloatArranger(ZfitArranger):
     def dump(self, rep: ZfitRepr) -> str:
         return str(rep.value)
 
-    def load(self, string: str) -> FloatRepr:
-        return FloatRepr(uid='float', value=float(string))
+    def load(self, struct: str) -> FloatRepr:
+        return FloatRepr(uid='float', value=float(struct))
 
 
 class BoolRepr(ZfitRepr):
@@ -84,8 +91,8 @@ class BoolArranger(ZfitArranger):
     def dump(self, rep: ZfitRepr) -> Dict['str', Any]:
         return str(rep.value)
 
-    def load(self, string: str) -> ZfitRepr:
-        return BoolRepr(uid='bool', value=bool(string))
+    def load(self, struct: str) -> ZfitRepr:
+        return BoolRepr(uid='bool', value=bool(struct))
 
 
 class IndependentParamRepr(ParamRepr):
@@ -119,12 +126,34 @@ class IndependentParamArranger(ParamArranger):
         floating = rep.fields.get('floating')
         if floating is not None:
             structure['floating'] = BoolArranger().dump()(floating)
+
         return structure
 
-    def load(self, string: str) -> IndependentParamRepr:
-        structure = super().load(string)
-        raise RuntimeError("TODO")
-        return IndependentParamRepr(uid='IndepParam', fields=structure)
+    def load(self, struct: str) -> IndependentParamRepr:
+        struct = struct.copy()
+
+        fields = {}
+
+        fields['value'] = FloatArranger().load(struct.pop('value'))
+        step_size = struct.get('step_size')
+        if step_size is not None:
+            fields['step_size'] = FloatArranger().load(step_size)
+
+        lower = struct.pop('lower', None)
+        if lower is not None:
+            fields['lower'] = FloatArranger().load(lower)
+
+        upper = struct.pop('upper', None)
+        if upper is not None:
+            fields['upper'] = FloatArranger().load(upper)
+
+        floating = struct.pop('floating', None)
+        if floating is not None:
+            fields['floating'] = BoolArranger().load()(floating)
+
+        rep = super().load(struct)
+        fields.update(rep.fields)
+        return IndependentParamRepr(uid=struct['type'], fields=fields)
 
 
 if __name__ == '__main__':
@@ -133,7 +162,14 @@ if __name__ == '__main__':
     param = zfit.Parameter('asdf', 10)
     param_repr = IndependentParamRepr(uid="IndepParam",
                                       fields={'name': StrRepr(uid='str', value=param.name),
-                                              'value': FloatRepr(uid='float', value=float(param.value()))})
+                                              'value': FloatRepr(uid='float', value=float(param.value())),
+                                              'step_size': FloatRepr(uid='float', value=float(param.step_size))
+                                              })
 
     dump = IndependentParamArranger().dump(param_repr)
     print(dump)
+    load_dump = IndependentParamArranger().load(dump)
+    print(load_dump)
+    # load_dump.fields['name'].value += '_loaded'
+    param_loaded = zfit.Parameter(**{k: rep.value for k, rep in load_dump.fields.items()})
+    print(param_loaded)
