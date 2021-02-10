2026e67c1edf4056931ccd4c187d712845806f3b
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-02-10 22:19:11 +0100

    WIP: adding criterion

--- zfit/minimizers/termination.py
+++ zfit/minimizers/termination.py
@@ -1,31 +1,33 @@
 #  Copyright (c) 2021 zfit
 import abc
-from typing import Optional
+from typing import Optional, Tuple
 
 import numpy as np
 
-from ..core.interfaces import ZfitLoss
-from ..util import ztyping
+from .fitresult import FitResult
 
 
 class ConvergenceCriterion(abc.ABC):
 
-    def __init__(self, tolerance: float, loss: ZfitLoss, params: ztyping.ParamTypeInput, name: str):
+    def __init__(self, tolerance: float, name: str):
         super().__init__()
-        if not isinstance(loss, ZfitLoss):
-            raise TypeError("loss has to be ZfitLoss")
-        self.loss = loss
         self.tolerance = tolerance
-        self.params = params
         self.name = name
 
-    def convergedV1(self, value, xvalues, grad=None, hesse=None, inv_hesse=None, **kwargs) -> bool:
-        self.last_value = self.calculateV1(value, xvalues=xvalues, grad=grad, hesse=hesse, inv_hesse=inv_hesse,
-                                           **kwargs)
-        return self.last_value < self.tolerance
+    def converged(self, result: FitResult) -> Tuple[bool, float]:
+        """Calculate the criterion and check if it is below the tolerance, return both.
+
+        Args:
+            result: Return the result which contains all the information
+
+        Returns:
+
+        """
+        value = self.calculate(result)
+        return value < self.tolerance, value
 
     @abc.abstractmethod
-    def calculateV1(self, value, xvalues, grad, hesse, inv_hesse, **kwargs):
+    def calculate(self, result: FitResult):
         raise NotImplementedError
 
 
@@ -35,10 +37,26 @@ def calculate_edm(grad, inv_hesse):
 
 class EDM(ConvergenceCriterion):
 
-    def __init__(self, tolerance: float, loss: ZfitLoss, params: ztyping.ParamTypeInput, name: Optional[str] = None):
+    def __init__(self, tolerance: float, name: Optional[str] = None):
         if name is None:
             name = "edm"
-        super().__init__(tolerance, loss, params, name)
+        super().__init__(tolerance=tolerance, name=name)
+
+    def calculate(self, result) -> float:
+
+        if callable(grad):
+            grad = grad()
+        if callable(hesse):
+            hesse = hesse()
+        if callable(inv_hesse):
+            inv_hesse = inv_hesse()
+        if inv_hesse is None:
+            if hesse is None:
+                raise RuntimeError("Need hesse or inv_hesse for convergence criterion:")
+            else:
+                inv_hesse = np.linalg.inv(hesse)
+        edm = calculate_edm(grad, inv_hesse)
+        return edm
 
     def calculateV1(self, value, xvalues, grad, hesse=None, inv_hesse=None, **kwargs) -> float:
         del value
