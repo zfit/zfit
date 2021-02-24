fix: wrong sign change in zfit_error

zfit/minimizers/errors.py
@@ -106,7 +106,7 @@ def compute_errors(result: "zfit.minimizers.fitresult.FitResult",
                     initial_values[d].append(ap_value)
 
             # TODO: improvement, use jacobian?
-            @np_cache(maxsize=25)
+            # @np_cache(maxsize=25)
             def func(values, args):
                 nonlocal ncalls
                 ncalls += 1
@@ -121,16 +121,21 @@ def compute_errors(result: "zfit.minimizers.fitresult.FitResult",
                                " caused by negative values returned from the PDF.")
                         raise FailEvalLossNaN(msg)
 
-                    shifted_loss = loss_value.numpy() - fmin - errordef * sigma ** 2
-                    gradients = np.array(gradients)
+                    zeroed_loss = loss_value.numpy() - fmin
 
-                if shifted_loss < -errordef - minimizer.tolerance:
-                    set_values(all_params, values)  # set values to the new minimum
-                    raise NewMinimum("A new is minimum found.")
+                    gradients = np.array(gradients)
                 if swap_sign(param):
-                    shifted_loss = - shifted_loss
+                    zeroed_loss = - zeroed_loss
                     gradients = - gradients
                     logging.info("Swapping sign in error calculation 'zfit_error'")
+
+                elif zeroed_loss < - minimizer.tolerance:
+                    set_values(all_params, values)  # set values to the new minimum
+                    raise NewMinimum("A new is minimum found.")
+
+                downward_shift = errordef * sigma ** 2
+                shifted_loss = zeroed_loss - downward_shift
+
                 return np.concatenate([[shifted_loss], gradients])
 
             to_return[param] = {}
@@ -144,8 +149,8 @@ def compute_errors(result: "zfit.minimizers.fitresult.FitResult",
                                       x0=initial_values[d],
                                       tol=rtol,
                                       options={
-                                          'factor': 10.,
-                                          'diag': param_scale,
+                                          # 'factor': 1.,
+                                          'diag': 1 / param_scale,
                                       },
                                       method=method)
                 to_return[param][d] = roots.x[all_params.index(param)] - param_value

zfit/minimizers/ipopt.py
@@ -257,22 +257,7 @@ class IPoptV1(BaseMinimizer):
 
         # ipopt_options['dual_inf_tol'] = TODO?
 
-        minimizer = ipyopt.Problem(**minimizer_kwargs
-                                   )
-
-        # set maximum number of iterations, also set in evaluator
-        # minimizer.set_maxeval(self.get_maxiter(len(params)))
-
-        # inv_hesse = None
-        # hessian_init = None
-        # init_scale = None
-        # if previous_result:
-        #     inv_hessian = previous_result.info.get('inv_hesse')
-        #     if inv_hessian is None:
-        #         hessian_init = previous_result.info.get('hesse')
-        #         if hessian_init is not None:
-        #             inv_hessian = np.linalg.inv(hessian_init)
-        #             init_scale = np.diag(inv_hessian)
+        minimizer = ipyopt.Problem(**minimizer_kwargs)
 
         minimizer.set(**{k: v for k, v in ipopt_options.items() if v is not None})
 
@@ -293,6 +278,7 @@ class IPoptV1(BaseMinimizer):
             # 'warm_start_same_structure',
             'warm_start_entire_iterate'
         )
+        minimizer.set_intermediate_callback(lambda *a, **k: print(a, k) or True)
 
         fmin = -999
         status = -999
@@ -328,6 +314,8 @@ class IPoptV1(BaseMinimizer):
                                                      edm=CRITERION_NOT_AVAILABLE,
                                                      evaluator=evaluator,
                                                      valid=valid,
+                                                     niter=None,
+                                                     criterion=criterion,
                                                      message=valid_message)
                 converged = criterion.converged(result_prelim) and valid
             criterion_value = criterion.last_value
@@ -366,6 +354,8 @@ class IPoptV1(BaseMinimizer):
                                     fmin=fmin,
                                     status=status,
                                     edm=edm,
+                                    criterion=criterion,
+                                    niter=None,
                                     converged=converged,
                                     evaluator=evaluator,
                                     valid=valid,