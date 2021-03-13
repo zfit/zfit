fix: use numpy_function instead of gradient taped py_function

tests/test_z.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2020 zfit
+#  Copyright (c) 2021 zfit
 
 import numpy as np
 import tensorflow as tf
@@ -14,7 +14,7 @@ def numpy_func(x, a):
 
 @z.function
 def wrapped_numpy_func(x_tensor, a_tensor):
-    result = z.py_function(func=numpy_func, inp=[x_tensor, a_tensor], Tout=tf.float64)
+    result = z.numpy_function(func=numpy_func, inp=[x_tensor, a_tensor], Tout=tf.float64)
     result = tf.sqrt(result)
     return result
 

zfit/util/execution.py
@@ -254,9 +254,9 @@ class RunManager:
             For example, multiplying by a constant (constant as in *not chaning ever*) does not require the constant to
             be a `tf.constant(...)` but can be a Python scalar. For example, it is also fine to use a fixed template shape
             using Numpy (Scipy etc), as the template shape will stay constant (this requires though to use a
-            `z.py_function` to work, but this is another story about graph mode or not).
+            `z.numpy_function` to work, but this is another story about graph mode or not).
 
-            To allow to have dynamic numpy operations in a component, preferably wrapped with `z.py_function` instead of
+            To allow to have dynamic numpy operations in a component, preferably wrapped with `z.numpy_function` instead of
             forced eager, and to still retrieve a meaningful gradient, a numerical gradient has to be used.
             In general, this can be achieved by setting the `autograd` to False. Any derivative received will then be
             numerically computed. Furthermore, some minimizers (e.g. :py:class:`~zfit.minimize.Minuit`) have their own way

zfit/z/math.py
@@ -59,7 +59,7 @@ def numerical_gradient(func: Callable, params: Iterable["zfit.Parameter"]) -> tf
     if tf.executing_eagerly():
         gradients = convert_to_tensor(grad_func(param_vals))
     else:
-        gradients = tf.py_function(grad_func, inp=[param_vals],
+        gradients = tf.numpy_function(grad_func, inp=[param_vals],
                                    Tout=tf.float64)
     if gradients.shape == ():
         gradients = tf.reshape(gradients, shape=(1,))
@@ -124,7 +124,7 @@ def numerical_hessian(func: Optional[Callable],
     if tf.executing_eagerly():
         computed_hessian = convert_to_tensor(hesse_func(param_vals))
     else:
-        computed_hessian = tf.py_function(hesse_func, inp=[param_vals],
+        computed_hessian = tf.numpy_function(hesse_func, inp=[param_vals],
                                           Tout=tf.float64)
     n_params = param_vals.shape[0]
     if hessian == 'diag':