9cfe915855000a285bd812ef7889cb89c3ffd28f
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-03-20 01:43:53 +0100

    fix: setup.py typo

--- setup.py
+++ setup.py
@@ -13,12 +13,6 @@ with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as requireme
 with open(os.path.join(here, 'requirements_dev.txt'), encoding='utf-8') as requirements_dev_file:
     requirements_dev = requirements_dev_file.read().splitlines()
 
-# split the developer requirements into setup and test requirements
-# if requirements_dev.count("") != 1 or requirements_dev.index("") == 0:
-#     raise SyntaxError("requirements_dev.txt has the wrong format: setup and test "
-#                       "requirements have to be separated by one blank line.")
-requirements_dev_split = requirements_dev.index("")
-
 setup(
     install_requires=requirements,
     extras_require={
