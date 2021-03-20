460512ab65db6ed03867947ae0d51decf888a18f
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-03-20 01:58:50 +0100

    fix: setup.cfg typo

--- setup.py
+++ setup.py
@@ -13,10 +13,22 @@ with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as requireme
 with open(os.path.join(here, 'requirements_dev.txt'), encoding='utf-8') as requirements_dev_file:
     requirements_dev = requirements_dev_file.read().splitlines()
 
+tests_require = [
+    'pytest>=3.4.2,<5.4',  # breaks unittests
+    'pytest-runner>=2.11.1',
+    'pytest-rerunfailures>=6',
+    'pytest-xdist',
+    'pytest-ordering',
+    'pytest-randomly',
+    'scikit-hep-testdata',
+    'pytest-timeout>=1',
+    'matplotlib'  # for plots in examples
+    ]
 setup(
     install_requires=requirements,
+    tests_require=tests_require,
     extras_require={
-        'dev': requirements_dev
+        'dev': requirements_dev + tests_require
     },
     use_scm_version=True,
 )
