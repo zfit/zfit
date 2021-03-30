a924f193c86d4585c4d7bd8e954bda6c2ace9569
SebastianJL <johannes.lade@yahoo.com>
2021-03-30 10:53:18 +0200

    Remove commented out code.

--- zfit/z/__init__.py
+++ zfit/z/__init__.py
@@ -7,32 +7,6 @@ Some function are already wrapped, others are not. Best practice is to use `z` w
 
 #  Copyright (c) 2021 zfit
 
-# fill the following in to the namespace for (future) wrapping
-
-# doesn't work below because of autoimport... probably anytime in the Future :)
-# from tensorflow import *  # Yes, this is wanted. Yields an equivalent z BUT we COULD wrap it :)
-# _module_dict = tensorflow.__dict__
-# try:
-#     _to_import = tensorflow.__all__
-# except AttributeError:
-#     _to_import = [name for name in _module_dict if not name.startswith('_')]
-#
-# __all__ = _to_import
-# del tensorflow
-# _imported = {}
-# _failed_imports = []
-# for _name in _to_import:
-#     try:
-#         _imported[_name] = _module_dict[_name]
-#     except KeyError as error:
-#         _failed_imports.append(_name)
-# if _failed_imports:
-#     warnings.warn("The following modules/attributes from TensorFlow could NOT be imported:\n{}".format(
-#     _failed_imports))
-# globals().update(_imported)
-#
-# del _imported, _failed_imports, _to_import, _module_dict
-
 from . import math, random, unstable
 from .wrapping_tf import (check_numerics, complex, convert_to_tensor, exp, pow,
                           random_normal, random_uniform, reduce_prod,
