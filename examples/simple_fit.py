05bfb59322a75aa351cc7a42809f6f219fc1cb6e
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-05-31 13:36:55 +0200

    fix: update np.array explicit object dtype

--- examples/simple_fit.py
+++ examples/simple_fit.py
@@ -4,29 +4,22 @@ import numpy as np
 
 import zfit
 
-# create space
 obs = zfit.Space("x", limits=(-2, 3))
 
-# parameters
 mu = zfit.Parameter("mu", 1.2, -4, 6)
 sigma = zfit.Parameter("sigma", 1.3, 0.5, 10)
-
-# model building, pdf creation
 gauss = zfit.pdf.Gauss(mu=mu, sigma=sigma, obs=obs)
 
-# data
 normal_np = np.random.normal(loc=2., scale=3., size=10000)
 data = zfit.Data.from_numpy(obs=obs, array=normal_np)
 
-# create NLL
 nll = zfit.loss.UnbinnedNLL(model=gauss, data=data)
 
-# # create a minimizer
 minimizer = zfit.minimize.Minuit()
 result = minimizer.minimize(nll)
 
-# do the error calculations with a hessian approximation
 param_errors = result.hesse()
-
-# or here with minos
 param_errors_asymetric = result.errors()
+
+a = param_errors
+x = param_errors_asymetric
