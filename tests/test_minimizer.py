cf4b730926835fd07d4f979926dec7de040efd98
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-06-03 14:58:25 +0200

    test: fix scipy minimizer unittests

--- tests/test_minimizer.py
+++ tests/test_minimizer.py
@@ -10,7 +10,6 @@ from ordered_set import OrderedSet
 
 import zfit.minimizers.optimizers_tf
 from zfit.minimizers.base_tf import WrapOptimizer
-from zfit.minimizers.baseminimizer import NOT_SUPPORTED
 from zfit.util.exception import OperationNotAllowedError
 
 true_mu = 4.5
@@ -72,13 +71,6 @@ def make_min_grad_hesse():
     return min_options
 
 
-def not_allowed(gradient, hessian):
-    from scipy.optimize import HessianUpdateStrategy
-    return (gradient in (True, '2-point', '3-point')
-            and not isinstance(hessian, HessianUpdateStrategy)
-            and hessian is not None)
-
-
 @pytest.mark.parametrize('minimizer_gradient_hessian', make_min_grad_hesse())
 # @pytest.mark.flaky(reruns=3)
 def test_scipy_derivative_options(minimizer_gradient_hessian):
@@ -91,11 +83,14 @@ def test_scipy_derivative_options(minimizer_gradient_hessian):
         kwargs['gradient'] = gradient
     if hessian is not None:
         kwargs['hessian'] = hessian
-    if not_allowed(gradient=gradient, hessian=hessian):
-        with pytest.raises(ValueError, match='Whenever the gradient is estimated via finite-differences'):
-            _ = minimizer_cls(**kwargs)
-        return  # end here
-    minimizer = minimizer_cls(**kwargs)
+
+    try:
+        minimizer = minimizer_cls(**kwargs)
+    except ValueError as error:  # we test a not allowed combination
+        if 'Whenever the gradient is estimated via finite-differences' in error.args[0]:
+            return
+        else:
+            raise
 
     result = minimizer.minimize(loss=loss)
     assert result.valid
