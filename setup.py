7765cd22517cd5c5f0e7a9115f6aae230b3951a2
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-08-14 13:53:02 +0200

    ci: fix typo3

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as requireme
 with open(os.path.join(here, 'requirements_dev.txt'), encoding='utf-8') as requirements_dev_file:
     requirements_dev = requirements_dev_file.read().splitlines()
 
+extras_require = {}
 if platform.system() != 'Darwin':  # OSX has no wheels for ipyopt, build fails
-    extras_require = {'ipyopt': ['ipyopt<0.12']}  # TODO: osx wheels? https://gitlab.com/g-braeunlich/ipyopt/-/issues/4
+    extras_require['ipyopt'] = ['ipyopt<0.12']  # TODO: osx wheels? https://gitlab.com/g-braeunlich/ipyopt/-/issues/4
 
 allreq = sum(extras_require.values(), [])
 
