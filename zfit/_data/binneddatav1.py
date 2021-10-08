8216f4153850d97f0313d771e54b45e97365c522
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-10-08 16:21:45 +0200

    docs: add docs for binned data

--- zfit/_data/binneddatav1.py
+++ zfit/_data/binneddatav1.py
@@ -2,14 +2,17 @@
 
 from __future__ import annotations
 
+from types import Union
+
 import boost_histogram as bh
 import hist
 import tensorflow as tf
 
 from zfit._variables.axis import histaxes_to_binning, binning_to_histaxes
 from zfit.core.interfaces import ZfitBinnedData, ZfitSpace
-from ..util.exception import ShapeIncompatibleError
 from zfit.z import numpy as znp
+from ..util import ztyping
+from ..util.exception import ShapeIncompatibleError
 
 
 # @tfp.experimental.auto_composite_tensor()
@@ -70,7 +73,7 @@ class BinnedDataV1(ZfitBinnedData,
 
     @classmethod  # TODO: add overflow bins if needed
     def from_tensor(cls, space: ZfitSpace, values: znp.array, variances: znp.array | None = None) -> BinnedDataV1:
-        """Create a Binned dataset defined in *space* where values are considered to be the counts.
+        """Create a binned dataset defined in *space* where values are considered to be the counts.
 
         Args:
             space: The space of the data. Variables need to match the values dimensions. The space has to be binned
@@ -88,6 +91,11 @@ class BinnedDataV1(ZfitBinnedData,
 
     @classmethod
     def from_hist(cls, hist: hist.NamedHist) -> BinnedDataV1:
+        """Create a binned dataset from a `hist` histogram.
+
+        Args:
+            hist: A NamedHist. The axes will be used as the binning in zfit.
+        """
         from zfit import Space
         space = Space(binning=histaxes_to_binning(hist.axes))
         values = znp.asarray(hist.values(flow=flow))
@@ -97,7 +105,12 @@ class BinnedDataV1(ZfitBinnedData,
         holder = BinnedHolder(space=space, values=values, variances=variances)
         return cls(holder=holder)
 
-    def with_obs(self, obs) -> BinnedDataV1:
+    def with_obs(self, obs: ztyping.ObsTypeInput) -> BinnedDataV1:
+        """Return a subset of the data in the ordering of *obs*.
+
+        Args:
+            obs: Which obs to return
+        """
         return type(self)(holder=self.holder.with_obs(obs))
 
     @property
@@ -106,6 +119,10 @@ class BinnedDataV1(ZfitBinnedData,
 
     @property
     def n_obs(self) -> int:
+        return self.rank
+
+    @property
+    def rank(self) -> int:
         return self.space.n_obs
 
     @property
@@ -113,6 +130,11 @@ class BinnedDataV1(ZfitBinnedData,
         return self.space.obs
 
     def to_hist(self) -> hist.NamedHist:
+        """Convert the binned data to a :py:class:`~hist.NamedHist`.
+
+        While a binned data object can be used inside zfit (PDFs,...), it lacks many convenience features that the `hist
+        library <https://hist.readthedocs.io/>`_ offers, such as plots.
+        """
         binning = binning_to_histaxes(self.holder.space.binning)
         h = hist.NamedHist(*binning, storage=bh.storage.Weight())
         h.view(flow=flow).value = self.values(flow=flow)
@@ -132,23 +154,51 @@ class BinnedDataV1(ZfitBinnedData,
 
     @property
     def axes(self):
+        return self.binning
+
+    @property
+    def binning(self):
         return self.space.binning
 
-    def values(self, flow=False):
+    def values(self) -> znp.array:  # , flow=False
+        """Values of the histogram as an ndim array.
+
+        Compared to `hist`, zfit does not make a difference between a view and a copy; tensors are immutable.
+        This distinction is made in the traced function by the compilation backend.
+
+        Returns:
+            Tensor of shape (nbins0, nbins1, ...) with nbins the number of bins in each observable.
+        """
         vals = self.holder.values
-        # if not flow:
+        # if not flow:  # TODO: flow?
         #     shape = tf.shape(vals)
         #     vals = tf.slice(vals, znp.ones_like(shape), shape - 2)
         return vals
 
-    def variances(self, flow=False):
+    def variances(self) -> Union[None, znp.array]:  # , flow=False
+        """Variances, if available, of the histogram as an ndim array.
+
+        Compared to `hist`, zfit does not make a difference between a view and a copy; tensors are immutable.
+        This distinction is made in the traced function by the compilation backend.
+
+        Returns:
+            Tensor of shape (nbins0, nbins1, ...) with nbins the number of bins in each observable.
+        """
         vals = self.holder.variances
-        # if not flow:
+        # if not flow:  # TODO: flow?
         #     shape = tf.shape(vals)
         #     vals = tf.slice(vals, znp.ones_like(shape), shape - 2)
         return vals
 
     def counts(self):
+        """Effective counts of the histogram as an ndim array.
+
+        Compared to `hist`, zfit does not make a difference between a view and a copy; tensors are immutable.
+        This distinction is made in the traced function by the compilation backend.
+
+        Returns:
+            Tensor of shape (nbins0, nbins1, ...) with nbins the number of bins in each observable.
+        """
         return self.values()
 
     # dummy
