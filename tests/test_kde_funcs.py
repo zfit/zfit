fb6ef6119df45e5bf69eba3cf16c9dd2b8f4743c
Sebastian Schmitt <sebastian19961025@web.de>
2021-11-13 18:47:28 +0100

    add tests with new limits logic for padreflect_data_weights_1dim

--- tests/test_kde_funcs.py
+++ tests/test_kde_funcs.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.mark.parametrize('weights', [None, np.array([1., 2., 3., 4., 5., 6., 7., 8.])])
 def test_padreflect_data_weights_1dim(weights):
     from zfit.models.kde import padreflect_data_weights_1dim
+    from zfit import Space
     testarray = np.array([0.1, 0.5, -2, 5, 6.7, -1.2, 1.2, 7])
 
     data, w = padreflect_data_weights_1dim(testarray, mode=0.1, weights=weights)
@@ -48,6 +49,31 @@ def test_padreflect_data_weights_1dim(weights):
     if weights is not None:
         true_w = np.sort(np.array([1., 2., 3., 4., 5., 6., 7., 8., 8, 5, 4]))
         np.testing.assert_allclose(true_w, np.sort(w))
+
+    testobs   = Space('testSpace', limits=(-2, 7.5))
+    data, w   = padreflect_data_weights_1dim(testarray, mode=0.1, limits=testobs.limits)
+    true_data = np.sort(np.array([-2.8, -2, 0.1, 0.5, -2, 5, 6.7, -1.2, 1.2, 7, 8.0, 8.3]))
+    np.testing.assert_allclose(true_data, np.sort(data))
+    if weights is not None:
+        true_w = np.sort(np.array([3, 6, 1, 2, 3, 4, 5, 6, 7, 8, 4, 5, 8]))
+
+    data, w   = padreflect_data_weights_1dim(testarray, mode=0.3, limits=testobs.limits)
+    true_data = np.sort(np.array([-4.5, -4.1, -2.8, -2, 0.1, 0.5, -2, 5, 6.7, -1.2, 1.2, 7, 8.0, 8.3, 10]))
+    np.testing.assert_allclose(true_data, np.sort(data))
+    if weights is not None:
+        true_w = np.sort(np.array([1, 2, 3, 6, 1, 2, 3, 4, 5, 6, 7, 8, 4, 5, 8]))
+
+    data, w   = padreflect_data_weights_1dim(testarray, mode={'lowermirror': 0.3}, limits=testobs.limits)
+    true_data = np.sort(np.array([-4.5, -4.1, -2.8, -2, 0.1, 0.5, -2, 5, 6.7, -1.2, 1.2, 7]))
+    np.testing.assert_allclose(true_data, np.sort(data))
+    if weights is not None:
+        true_w = np.sort(np.array([1, 2, 3, 6, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+    data, w   = padreflect_data_weights_1dim(testarray, mode={'uppermirror': 0.3}, limits=testobs.limits)
+    true_data = np.sort(np.array([0.1, 0.5, -2, 5, 6.7, -1.2, 1.2, 7, 8.0, 8.3, 10]))
+    np.testing.assert_allclose(true_data, np.sort(data))
+    if weights is not None:
+        true_w = np.sort(np.array([1, 2, 3, 4, 5, 6, 7, 8, 4, 5, 8]))
 
     with pytest.raises(ValueError):
         data, w = padreflect_data_weights_1dim(testarray, mode={'uper': 0.3}, weights=weights)
