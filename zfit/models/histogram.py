a4c479481d39bf653bafb33e80f270e4254d875f
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2021-11-30 12:56:14 +0100

    docs: add docs to histogrampdf

--- zfit/models/histogram.py
+++ zfit/models/histogram.py
@@ -1,17 +1,51 @@
 #  Copyright (c) 2021 zfit
+from typing import Optional
+
 import numpy as np
 from uhi.typing.plottable import PlottableHistogram
-import zfit.z.numpy as znp
 
-from ..core.space import supports
+import zfit.z.numpy as znp
 from ..core.binnedpdf import BaseBinnedPDFV1
 from ..core.interfaces import ZfitBinnedData
+from ..core.space import supports
+from ..util import ztyping
 from ..util.exception import SpecificFunctionNotImplemented
 
 
 class HistogramPDF(BaseBinnedPDFV1):
 
-    def __init__(self, data, extended=None, norm=None, name="BinnedTemplatePDF"):
+    def __init__(
+            self,
+            data: ztyping.BinnedDataInputType,
+            extended: Optional[ztyping.ExtendedInputType] = None,
+            norm: Optional[ztyping.NormInputType] = None,
+            name: str = "HistogramPDF"
+    ) -> None:
+        """Binned PDF resembling a histogram.
+
+        Simple histogram PDF that can be used to model a histogram as a PDF.
+
+
+        Args:
+            data: Histogram to be used as PDF.
+            extended: |@doc:pdf.init.extended| The overall yield of the PDF.
+               If this is parameter-like, it will be used as the yield,
+               the expected number of events, and the PDF will be extended.
+               An extended PDF has additional functionality, such as the
+               `ext_*` methods and the `counts` (for binned PDFs). |@docend:pdf.init.extended|
+               |@doc:pdf.init.extended.auto|
+               If this is set to `True`, the PDF will be extended automatically
+               using the total number of events in the histogram.
+               This is the default. |@docend:pdf.init.extended.auto|
+            norm: |@doc:pdf.init.norm| Normalization of the PDF.
+               By default, this is the same as the default space of the PDF. |@docend:pdf.init.norm|
+            name: |@doc:model.init.name| Human-readable name
+               or label of
+               the PDF for better identification.
+               Has no programmatical functional purpose as identification. |@docend:model.init.name|
+        """
+        if extended is None:
+            extended = True
         if not isinstance(data, ZfitBinnedData):
             if isinstance(data, PlottableHistogram):
                 from zfit._data.binneddatav1 import BinnedData
