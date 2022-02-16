2959332076b4d4bbefd177bb46143eb96eba7f9a
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2022-02-16 23:02:41 +0100

    tests: add test for multinomial fix

--- tests/test_z.py
+++ tests/test_z.py
@@ -21,3 +21,20 @@ def test_wrapped_func():
     rnd = z.random.uniform(shape=(10,))
     result = wrapped_numpy_func(rnd, z.constant(3.))
     np.testing.assert_allclose(rnd * np.sqrt(3), result)
+
+
+def test_multinomial():
+    import zfit.z.numpy as znp
+
+    probs = np.array([0.5, 0.5])
+    x = z.random.counts_multinomial(10000, probs=probs)
+    assert x.shape == (2,)
+    probs = np.array([0.1, 0.2, 0.7])
+    x = z.random.counts_multinomial(10000, probs=probs)
+    assert x.shape == (3,)
+    probs = np.array([0.5, 0.5])
+    x = z.random.counts_multinomial(10000, probs=probs, dtype=znp.float64)
+    assert x.shape == (2,)
+    probs = np.array([0.1, 0.7, 0.2])
+    x = z.random.counts_multinomial(10000, probs=probs, dtype=znp.float64)
+    assert x.shape == (3,)
