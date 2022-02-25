1276a5914456d4a1855189dc4a68e9b840bf53ce
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2022-02-25 12:43:24 +0100

    fix: add error message if n is None for sampling

--- zfit/core/basemodel.py
+++ zfit/core/basemodel.py
@@ -950,6 +950,8 @@ class BaseModel(BaseNumeric, GraphCachable, BaseDimensional, ZfitModel):
             InvalidArgumentError: if n is not specified and pdf is not extended.
         """
         if not isinstance(n, str):
+            if n is None:
+                raise ValueError("n has to be specified if the pdf is not extended.")
             n = tf.convert_to_tensor(n)
             n = tf.cast(n, dtype=tf.int32)
 
