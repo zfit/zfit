333f3d42c91a6ea156b6dd76a2da791c974a15ee
maria <ermolaeva.mv@phystech.edu>
2022-06-11 21:59:52 +0300

    code review fixes

--- tests/test_cacheable_pdf.py
+++ tests/test_cacheable_pdf.py
@@ -1,20 +1,25 @@
-import pytest
 import zfit
 import zfit.z.numpy as znp
 import tensorflow as tf
 from zfit.models.cache import CacheablePDF
 
-mu = zfit.Parameter('mu', 1., -5, 5)
-sigma = zfit.Parameter('sigma', 1, 0, 10)
-obs = zfit.Space('x', limits=[-5., 5.])
-gauss = zfit.pdf.Gauss(mu=mu, sigma=sigma, obs=obs)
-cached_gauss = CacheablePDF(gauss)
-x = znp.linspace(-5, 5, 500)
-
 
 def test_pdf():
+    mu = zfit.Parameter('mu', 1., -5, 5)
+    sigma = zfit.Parameter('sigma', 1, 0, 10)
+    obs = zfit.Space('x', limits=[-5., 5.])
+    gauss = zfit.pdf.Gauss(mu=mu, sigma=sigma, obs=obs)
+    cached_gauss = CacheablePDF(gauss)
+    x = znp.linspace(-5, 5, 500)
+
     assert tf.math.reduce_all(tf.equal(gauss.pdf(x), cached_gauss.pdf(x)))
 
 
 def test_integrate():
+    mu = zfit.Parameter('mu', 1., -5, 5)
+    sigma = zfit.Parameter('sigma', 1, 0, 10)
+    obs = zfit.Space('x', limits=[-5., 5.])
+    gauss = zfit.pdf.Gauss(mu=mu, sigma=sigma, obs=obs)
+    cached_gauss = CacheablePDF(gauss)
+
     assert tf.math.reduce_all(tf.equal(gauss.integrate(limits=obs), cached_gauss.integrate(limits=obs)))
--- zfit/models/cache.py
+++ zfit/models/cache.py
@@ -22,56 +22,57 @@ class CacheablePDF(BaseFunctor):
     def __init__(self, pdf, cache_tol=None, **kwargs):
         super().__init__(pdfs=pdf, obs=pdf.space, **kwargs)
         params = list(pdf.get_params())
-        self.cached_pdf_params = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(params))), trainable=False,
-                                             validate_shape=False, dtype=tf.float64)
-        self.cached_pdf_params_for_integration = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(params))),
-                                                             trainable=False,
-                                                             validate_shape=False, dtype=tf.float64)
-        self.pdf_cache = None
-        self.cached_integral_limits = None
-        self.integral_cache = None
-        self.integral_cache_valid = tf.Variable(initial_value=False, trainable=False)
+        self._cached_pdf_params = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(params))), trainable=False,
+                                              validate_shape=False, dtype=tf.float64)
+        self._cached_pdf_params_for_integration = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(params))),
+                                                              trainable=False,
+                                                              validate_shape=False, dtype=tf.float64)
+        self._pdf_cache = None
+        self._cached_integral_limits = None
+        self._integral_cache = None
+        self._integral_cache_valid = tf.Variable(initial_value=False, trainable=False)
 
-        self.cache_tolerance = 1e-8 if cache_tol is None else cache_tol
+        self._cache_tolerance = 1e-8 if cache_tol is None else cache_tol
 
     @supports(norm="space")
-    def _pdf(self, x, norm_range):
-        if self.pdf_cache is None:
-            self.pdf_cache = tf.Variable(znp.zeros(shape=tf.shape(x)[0]), trainable=False, validate_shape=False,
-                                         dtype=tf.float64)
+    def _pdf(self, x, norm):
+        if self._pdf_cache is None:
+            self._pdf_cache = tf.Variable(znp.zeros(shape=tf.shape(x)[0]), trainable=False, validate_shape=False,
+                                          dtype=tf.float64)
 
         pdf_params = list(self.pdfs[0].get_params())
         stacked_pdf_params = tf.stack(pdf_params)
         params_same = tf.math.reduce_all(
-            tf.math.abs(stacked_pdf_params - self.cached_pdf_params) < self.cache_tolerance)
-        self.cached_pdf_params.assign(stacked_pdf_params, read_value=False)
-        pdf = get_value(self.pdf_cache, params_same, lambda: self.pdfs[0].pdf(x, norm_range))
+            tf.math.abs(stacked_pdf_params - self._cached_pdf_params) < self._cache_tolerance)
+        self._cached_pdf_params.assign(stacked_pdf_params, read_value=False)
+        pdf = get_value(self._pdf_cache, params_same, lambda: self.pdfs[0].pdf(x, norm))
         return pdf
 
     @supports(norm="space")
     def _integrate(self, limits, norm, options=None):
-        if self.cached_integral_limits is None:
-            self.cached_integral_limits = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(limits.limits))),
-                                                      trainable=False, validate_shape=False, dtype=tf.float64)
+        if self._cached_integral_limits is None:
+            self._cached_integral_limits = tf.Variable(znp.zeros(shape=tf.shape(tf.stack(limits.limits))),
+                                                       trainable=False, validate_shape=False, dtype=tf.float64)
 
-        if self.integral_cache is None:
-            self.integral_cache = tf.Variable(znp.zeros(shape=tf.shape([1])), trainable=False, validate_shape=False,
-                                              dtype=tf.float64)
+        if self._integral_cache is None:
+            self._integral_cache = tf.Variable(znp.zeros(shape=tf.shape([1])), trainable=False, validate_shape=False,
+                                               dtype=tf.float64)
 
         pdf_params = list(self.pdfs[0].get_params())
         stacked_pdf_params = tf.stack(pdf_params)
         params_same = tf.math.reduce_all(
-            tf.math.abs(stacked_pdf_params - self.cached_pdf_params_for_integration) < self.cache_tolerance)
+            tf.math.abs(stacked_pdf_params - self._cached_pdf_params_for_integration) < self._cache_tolerance)
 
         stacked_integral_limits = tf.stack(limits.limits)
         limits_same = tf.math.reduce_all(
-            tf.math.abs(stacked_integral_limits - self.cached_integral_limits) < self.cache_tolerance)
+            tf.math.abs(stacked_integral_limits - self._cached_integral_limits) < self._cache_tolerance)
 
-        self.integral_cache_valid.assign(tf.math.logical_and(params_same, limits_same), read_value=False)
+        self._integral_cache_valid.assign(tf.math.logical_and(params_same, limits_same), read_value=False)
 
-        self.cached_pdf_params_for_integration.assign(stacked_pdf_params, read_value=False)
-        self.cached_integral_limits.assign(stacked_integral_limits, read_value=False)
+        self._cached_pdf_params_for_integration.assign(stacked_pdf_params, read_value=False)
+        self._cached_integral_limits.assign(stacked_integral_limits, read_value=False)
 
-        integral = get_value(self.integral_cache, self.integral_cache_valid,
+        integral = get_value(self._integral_cache, self._integral_cache_valid,
                              lambda: self.pdfs[0].integrate(limits, norm, options=None))
         return integral
+
