tests: fix wrong shapes in pytest.approx

tests/test_loss.py
@@ -320,8 +320,8 @@ def test_gradients(chunksize):
     param1.set_value(initial1)
     param2.set_value(initial2)
     gradient3 = nll.gradient()
-    assert frozenset(g.numpy() for g in gradient3) == pytest.approx(
-        frozenset(both_gradients_true)
+    assert np.testing.assert_allclose(
+        frozenset(both_gradients_true), frozenset(g.numpy() for g in gradient3)
     )
 
 

tests/test_pdf_cb.py
@@ -72,7 +72,7 @@ def test_cb_integral():
 
     integral = np.sum(integrals)
     integral_full = zfit.run(cbl.integrate(bounds, norm=False))
-    assert pytest.approx(integral_full, integral)
+    assert pytest.approx(float(integral_full)) == float(integral)
 
 
 def test_cb_dcb():
@@ -149,4 +149,4 @@ def test_cb_dcb():
 
         integral = np.sum(integrals)
         integral_full = zfit.run(dcb.integrate((bounds[0], up), norm=False))
-        assert pytest.approx(integral_full) == integral
+        assert pytest.approx(float(integral_full)) == float(integral)

tests/test_pdf_sumpdf.py
@@ -61,8 +61,9 @@ def test_frac_behavior(yields):
         assert len(sumpdf2.params) == 3
         assert sumpdf2.params["frac_0"] == frac1
         assert sumpdf2.params["frac_1"] == frac2
-        assert pytest.approx(
-            frac3.value().numpy(), sumpdf2.params["frac_2"].value().numpy()
+        assert (
+            pytest.approx(frac3.value().numpy())
+            == sumpdf2.params["frac_2"].value().numpy()
         )
         assert not sumpdf1.is_extended
 
@@ -162,4 +163,4 @@ def test_integrate():
 
     integral = np.sum(integrals)
     integral_full = zfit.run(sumpdf.integrate((lower, upper), norm=False))
-    assert pytest.approx(integral_full) == integral
+    assert pytest.approx(float(integral_full)) == float(integral)