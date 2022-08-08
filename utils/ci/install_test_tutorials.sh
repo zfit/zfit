b25b33745f88c8f4532335219e39771dbae196c9
Jonas Eschle <mayou36@jonas.eschle.com>
2022-08-08 14:18:46 +0200

    ci: upgrade ci to python 3.10

--- utils/ci/install_test_tutorials.sh
+++ utils/ci/install_test_tutorials.sh
@@ -16,7 +16,7 @@ mkdir -p tmp_test_zfit_tutorials && cd tmp_test_zfit_tutorials || exit 1
 git clone https://github.com/zfit/zfit-tutorials.git
 pip install nbval
 pip install -r zfit-tutorials/requirements.txt 2>&1 | tail -n 11 &&
-pytest --nbval-lax zfit-tutorials --ignore=zfit-tutorials/experimental
+pytest --nbval-lax zfit-tutorials --ignore=zfit-tutorials/experimental --ignore=zfit-tutorials/_website --ignore=zfit-tutorials/_unused
 cd -
 rm -rf tmp_test_zfit_tutorials
 deactivate
