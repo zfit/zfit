0219ca778ea07076271d744a4572357accaa1a12
iasonkrom <iason.krom@gmail.com>
2024-03-13 14:06:54 +0100

    add tests for faddeeva function

--- tests/test_z_numpy.py
+++ tests/test_z_numpy.py
@@ -1,6 +1,9 @@
 #  Copyright (c) 2022 zfit
+import numpy as np
 import tensorflow as tf
+from scipy.special import wofz
 
+from zfit import z
 import zfit.z.numpy as znp
 
 
@@ -14,3 +17,50 @@ def test_z_numpy_ndarray_is_tensorflow_tensor():
     assert znp.ndarray is tf.Tensor
     assert isinstance(znp.array(1), tf.Tensor)
     assert isinstance(znp.sum(znp.array(0)), tf.Tensor)
+
+
+def test_faddeeva():
+    """We only test with positive imaginary part as the Humlicek approximation of the Faddeeva function is becoming
+    unstable in the negative imaginary part region.
+
+    That is also due to the nature of the Faddeeva function in that region.
+    """
+    size = int(1e3)
+    test_values1 = np.random.uniform(0, 0.1, size) + 1.0j * np.random.uniform(
+        0, 0.1, size
+    )
+    test_values2 = np.random.uniform(0, 1, size) + 1.0j * np.random.uniform(0, 1, size)
+    test_values3 = np.random.uniform(0, 10, size) + 1.0j * np.random.uniform(
+        0, 10, size
+    )
+    test_values4 = np.random.uniform(0, 100, size) + 1.0j * np.random.uniform(
+        0, 100, size
+    )
+    test_values5 = np.random.uniform(-0.1, 0, size) + 1.0j * np.random.uniform(
+        0, 1, size
+    )
+    test_values6 = np.random.uniform(-1, 0, size) + 1.0j * np.random.uniform(0, 1, size)
+    test_values7 = np.random.uniform(-10, 0, size) + 1.0j * np.random.uniform(
+        0, 10, size
+    )
+    test_values8 = np.random.uniform(-100, 0, size) + 1.0j * np.random.uniform(
+        0, 100, size
+    )
+    test_values = np.concatenate(
+        [
+            test_values1,
+            test_values2,
+            test_values3,
+            test_values4,
+            test_values5,
+            test_values6,
+            test_values7,
+            test_values8,
+        ]
+    )
+    assert znp.allclose(
+        znp.faddeeva_humlicek(znp.asarray(test_values)),
+        wofz(test_values),
+        rtol=1e-06,
+        atol=1e-10,
+    )
