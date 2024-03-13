fb0bab38c8c8de66ed77ac794de85e2a2cd696c5
iasonkrom <iason.krom@gmail.com>
2024-03-13 22:06:28 +0100

    test faddeeva gradient even more

--- tests/test_z_numpy.py
+++ tests/test_z_numpy.py
@@ -25,7 +25,7 @@ def test_faddeeva():
 
     That is also due to the nature of the Faddeeva function in that region.
     """
-    size = int(1e3)
+    size = int(1e4)
     test_values1 = np.random.uniform(0, 0.1, size) + 1.0j * np.random.uniform(
         0, 0.1, size
     )
@@ -68,3 +68,24 @@ def test_faddeeva():
         result = znp.faddeeva_humlicek(test_values_tensor)
         gradientstf = tape.gradient(result, test_values_tensor)
     assert np.all(np.isfinite(gradientstf))
+
+    # we test on a small sample of size 10 because the numerical gradient is slow to compute
+    # we restrict our selves to real x and not go far away from 0 (-1, 1) because the Faddeeva function flattens out and gradients become zero.
+    # we test the real and imaginary part separately because numdifftools can't differentiate with respect to complex numbers.
+    x = np.random.uniform(-1, 1, size=10)
+    f_real = lambda x: znp.real(znp.faddeeva_humlicek(x))
+    f_imag = lambda x: znp.imag(znp.faddeeva_humlicek(x))
+
+    num_gradients_real = z.math.numdifftools.Gradient(f_real)(x)
+    num_gradients_imag = z.math.numdifftools.Gradient(f_imag)(x)
+
+    x = znp.asarray(x)
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(x)
+        result_real = f_real(x)
+        result_imag = f_imag(x)
+        tf_gradients_real = tape.gradient(result_real, x)
+        tf_gradients_imag = tape.gradient(result_imag, x)
+
+    np.testing.assert_allclose(np.diag(num_gradients_real), tf_gradients_real)
+    np.testing.assert_allclose(np.diag(num_gradients_imag), tf_gradients_imag)
