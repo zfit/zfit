cd5121cb91f8fb724c4c444b1de0924830dafc9d
Jonas Eschle <mayou36@jonas.eschle.com>
2024-04-01 12:41:27 -0400

    Update zfit/models/basic.py

--- zfit/models/basic.py
+++ zfit/models/basic.py
@@ -308,7 +308,19 @@ class Voigt(BasePDF, SerializableMixin):
         complex_z = (x + 1j * gamma_complex) / (np.sqrt(2) * sigma_complex)
         return znp.real(znp.faddeeva_humlicek(complex_z)) / (sigma * np.sqrt(2 * np.pi))
 
+def _voigt_integral_from_inf_to_inf(limits, params, model):
+    del model, limits  # unused, fixed limits
+    params["m"]
+    sigma = params["sigma"]
+    params["gamma"]
+    return sigma * np.sqrt(2 * np.pi)
 
+
+limits = Space(axes=0, limits=(-znp.inf, znp.inf))
+# todo: this only works if executing eagerly, which fails for at least the binned PDFs
+# possible solution comes with the `space.static` concept (?) and a StaticLimitsNotAvailable error,
+# falling back to whatever is available
+#  Voigt.register_analytic_integral(func=_voigt_integral_from_inf_to_inf, limits=limits)
 class VoigtPDFRepr(BasePDFRepr):
     _implementation = Voigt
     hs3_type: Literal["Voigt"] = Field("Voigt", alias="type")
