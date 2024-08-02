0436dec591924a4a4f4656008d7b524fed7d431c
Jonas Eschle <mayou36@jonas.eschle.com>
2024-08-02 10:36:46 -0400

    chore: format some code
    Signed-off-by: Jonas Eschle <mayou36@jonas.eschle.com>

--- zfit/core/loss.py
+++ zfit/core/loss.py
@@ -1145,8 +1145,8 @@ class ExtendedUnbinnedNLL(BaseUnbinnedNLL):
             nevents = dat.n_events if dat.weights is None else z.reduce_sum(dat.weights)
             nevents = znp.asarray(nevents, tf.float64)
             nevents_collected.append(nevents)
-            yields.append(mod.get_yield())
-        yields = znp.stack(yields, axis=0)
+            yields.append(znp.atleast_1d(mod.get_yield()))
+        yields = znp.concatenate(yields, axis=0)
         nevents_collected = znp.stack(nevents_collected, axis=0)
 
         term_new = tf.nn.log_poisson_loss(nevents_collected, znp.log(yields), compute_full_loss=log_offset is False)
