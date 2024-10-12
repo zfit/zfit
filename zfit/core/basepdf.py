be85f13af2e656beee6aa3719277fc890bd7bdbb
jonas-eschle <Jonas.Eschle@cern.ch>
2024-10-12 00:51:04 -0400

    chore: cleanup core
    Signed-off-by: jonas-eschle <Jonas.Eschle@cern.ch>

--- zfit/core/basepdf.py
+++ zfit/core/basepdf.py
@@ -55,6 +55,8 @@ also the advanced models in `zfit models <https://github.com/zfit/zfit-tutorials
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Iterable, Optional
+
 from collections.abc import Iterable
 from typing import TYPE_CHECKING, Optional
 
